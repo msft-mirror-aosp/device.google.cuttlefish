f6601f9c6ac9124ce320820be5f9bba109b50345
Greg Hartman <ghartman@google.com>
2019-02-20 11:14:48 -0800

    Revert "Add host packages for host-side GPU"
    This reverts commit b6831f384e4cd2142e600edb59c162f7f3b46c26.
    
    We no longer want the emulator's pipeline code
    
    BUG: 113617962
    BUG: 117561734
    BUG: 118705599
    Test: Local build
    Change-Id: I298bd5a2daed7714a3c572bbff23f44c1bd312a3

--- shared/device.mk
+++ shared/device.mk
@@ -100,7 +100,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/google/cuttlefish/shared/config/README_egl.md:$(TARGET_COPY_OUT_VENDOR)/lib/egl/README.md
 
-# Guest side SwiftShader
+# SwiftShader provides a software-only implementation that is not thread-safe
 PRODUCT_PACKAGES += \
     libEGL_swiftshader \
     libGLESv1_CM_swiftshader \
@@ -108,18 +108,10 @@ PRODUCT_PACKAGES += \
 
 # The locking_wrapper puts a lock around all of the SwiftShader entry points to
 # serialize all access to the library, reducing the chances for races.
-# Applies only to guest-side SwiftShader
 PRODUCT_PACKAGES += \
     libEGL_locking_wrapper \
     libGLESv1_CM_locking_wrapper \
     libGLESv2_locking_wrapper
-
-# # GLES encoder, reused from goldfish
-# Disabled to prevent a potential race that would disable ASAN b/120988351
-# PRODUCT_PACKAGES += \
-#     libEGL_emulation \
-#     libGLESv1_CM_emulation \
-#     libGLESv2_emulation \
 
 DEVICE_PACKAGE_OVERLAYS := device/google/cuttlefish/shared/overlay
 PRODUCT_AAPT_CONFIG := normal large xlarge hdpi xhdpi
