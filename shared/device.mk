88123ff67a467b1a7f22348cc2472ee7e3170578
Jorge E. Moreira <jemoreira@google.com>
2019-04-01 23:22:10 -0700

    Disable Setup Wizard at build time instead of boot
    Bug: 129689270
    Test: run locally
    Change-Id: Id8768e23c14977b000600da23bbad896538d6b8d

--- shared/device.mk
+++ shared/device.mk
@@ -28,6 +28,7 @@ PRODUCT_PRODUCT_PROPERTIES := \
     persist.adb.tcp.port=5555 \
     persist.traced.enable=1 \
     ro.com.google.locationfeatures=1 \
+    ro.setupwizard.mode=DISABLED \
  
 # Explanation of specific properties:
 #   debug.hwui.swap_with_damage avoids boot failure on M http://b/25152138
