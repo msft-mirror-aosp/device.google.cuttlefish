4782b459e10b6aad1c70a11f8fd0fc8e16443787
Jorge E. Moreira <jemoreira@google.com>
2019-08-20 16:14:26 -0700

    Delete gsi target
    Bug: 139763357
    Test: none
    Change-Id: Iaee840b1fc854394fbb9f7b2725cd281ca3d1092
    (cherry picked from commit be0c5c5b53bf34472fba6769aca9c87ff9742721)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -22,7 +22,6 @@ PRODUCT_MAKEFILES := \
 	aosp_cf_x86_phone:$(LOCAL_DIR)/vsoc_x86/phone/aosp_cf.mk \
 	aosp_cf_x86_go_phone:$(LOCAL_DIR)/vsoc_x86/go_phone/device.mk \
 	aosp_cf_x86_go_512_phone:$(LOCAL_DIR)/vsoc_x86/go_512_phone/device.mk \
-	aosp_cf_x86_gsi:$(LOCAL_DIR)/vsoc_x86/gsi/aosp_cf_x86_gsi.mk \
 	aosp_cf_x86_tv:$(LOCAL_DIR)/vsoc_x86/tv/device.mk
 
 COMMON_LUNCH_CHOICES := \
