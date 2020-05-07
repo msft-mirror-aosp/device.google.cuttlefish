dfa38430d5124568748af8b4e950e50ed29daf14
Alistair Delva <adelva@google.com>
2020-05-07 10:45:54 -0700

    Add ambient_temperature
    Add some missing sensors that the new reference HAL supports
    
    Bug: 155989875
    Bug: 170364480
    Change-Id: I2ed8ba758779bc322b486a31b58cab46a80e67ff
    Merged-In: I2ed8ba758779bc322b486a31b58cab46a80e67ff
    (cherry picked from commit ba04c50301d4121b42cad0797dada277ab3e520d)
    [adelva: no relative_humidity sensor on P]

--- shared/device.mk
+++ shared/device.mk
@@ -133,6 +133,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.ambient_temperature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.ambient_temperature.xml \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
