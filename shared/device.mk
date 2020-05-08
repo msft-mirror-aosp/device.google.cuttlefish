c37f41979614ba38eb4809690c180984972a0708
A. Cody Schuffelen <schuffelen@google.com>
2020-05-07 22:34:07 -0700

    Switch to host keymaster
    Bug: 155328335
    Test: launch_cvd
    Change-Id: Ib54d7e80f0d134e4717a56ea6005cd69042ec928
    (cherry picked from commit c2bddd1d79872877d170496b1bb87bb3c5223c15)

--- shared/device.mk
+++ shared/device.mk
@@ -380,7 +380,7 @@ PRODUCT_PACKAGES += \
 # Keymaster HAL
 #
 PRODUCT_PACKAGES += \
-     android.hardware.keymaster@4.1-service
+     android.hardware.keymaster@4.1-service.remote
 
 #
 # Power HAL
