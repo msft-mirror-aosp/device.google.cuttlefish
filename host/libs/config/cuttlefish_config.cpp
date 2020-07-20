google/android-cuttlefish

Switch logcat_receiver from vsock to virtio-console
Remove "logcat_port" and replace it with "logcat_pipe_name". Alter the
logcat_receiver to read from a pipe instead of a vsocket. Update the
crosvm and qemu backends to create the hvc2 serial port and update the
init script to write to this new location.

In the guest, logcat can now run as logd, which fixes some long-standing
sepolicy bug_maps. Keep logcat in the root group, so it can access all
the logpersist classes. Remove extraneous sepolicy for writing to a
pipe.

Bug: 155217436
Bug: 160341724
Test: launch_cvd
Test: launch_cvd -vm_manager=qemu_cli
Change-Id: Ic1450da7977d90f7fa01b4c40a661ea60504c239

cvd/host/libs/config/cuttlefish_config.cpp
@@ -147,7 +147,6 @@ const char* kDataPolicy = "data_policy";
 const char* kBlankDataImageMb = "blank_data_image_mb";
 const char* kBlankDataImageFmt = "blank_data_image_fmt";
 
-const char* kLogcatPort = "logcat_port";
 const char* kLogcatReceiverBinary = "logcat_receiver_binary";
 const char* kConfigServerPort = "config_server_port";
 const char* kConfigServerBinary = "config_server_binary";
@@ -393,6 +392,10 @@ std::string CuttlefishConfig::InstanceSpecific::kernel_log_pipe_name() const {
 
 std::string CuttlefishConfig::InstanceSpecific::console_pipe_name() const {
   return cuttlefish::AbsolutePath(PerInstanceInternalPath("console-pipe"));
+}
+
+std::string CuttlefishConfig::InstanceSpecific::logcat_pipe_name() const {
+  return cuttlefish::AbsolutePath(PerInstanceInternalPath("logcat-pipe"));
 }
 
 bool CuttlefishConfig::deprecated_boot_completed() const {
@@ -679,14 +682,6 @@ int CuttlefishConfig::InstanceSpecific::vehicle_hal_server_port() const {
 
 void CuttlefishConfig::MutableInstanceSpecific::set_vehicle_hal_server_port(int vehicle_hal_server_port) {
   (*Dictionary())[kVehicleHalServerPort] = vehicle_hal_server_port;
-}
-
-int CuttlefishConfig::InstanceSpecific::logcat_port() const {
-  return (*Dictionary())[kLogcatPort].asInt();
-}
-
-void CuttlefishConfig::MutableInstanceSpecific::set_logcat_port(int logcat_port) {
-  (*Dictionary())[kLogcatPort] = logcat_port;
 }
 
 int CuttlefishConfig::InstanceSpecific::config_server_port() const {

msft-mirror-aosp/device.google.cuttlefish

Merged-In: Ic1450da7977d90f7fa01b4c40a661ea60504c239
(cherry picked from commit 4c20aa778f7a97be50284237d9afea1394b82320)

host/libs/config/cuttlefish_config.cpp
@@ -370,6 +370,10 @@ std::string CuttlefishConfig::InstanceSpecific::console_pipe_name() const {
   return cvd::AbsolutePath(PerInstanceInternalPath("console-pipe"));
 }
 
+std::string CuttlefishConfig::InstanceSpecific::logcat_pipe_name() const {
+  return cvd::AbsolutePath(PerInstanceInternalPath("logcat-pipe"));
+}
+
 bool CuttlefishConfig::deprecated_boot_completed() const {
   return (*dictionary_)[kDeprecatedBootCompleted].asBool();
 }