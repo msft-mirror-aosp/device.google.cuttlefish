google/android-cuttlefish

Replace unix socket with pty
Replace the unix socket created by console_forwarder with a UNIX PTY.
This enables true terminal emulators to be connected to cuttlefish, so
that terminal control messages are handled correctly.

Another benefit is that GDB can connect to a PTY, so we can now use
KGDB over the Android serial console port.

Bug: 160893516
Bug: 161416426
Change-Id: I1b65e1cc1f6a1f74c9e296e8ab6af3fbe6ab164e

host/commands/console_forwarder/main.cpp
@@ -14,7 +14,10 @@
  * limitations under the License.
  */
 
+#include <termios.h>
+#include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 
 #include <deque>
 #include <thread>
@@ -35,9 +38,9 @@ DEFINE_int32(console_out_fd,
              -1,
              "File descriptor for the console's output channel");
 
-// Handles forwarding the serial console to a socket.
-// It receives the socket fd along with a couple of fds for the console (could
-// be the same fd twice if, for example a socket_pair were used).
+// Handles forwarding the serial console to a pseudo-terminal (PTY)
+// It receives a couple of fds for the console (could be the same fd twice if,
+// for example a socket_pair were used).
 // Data available in the console's output needs to be read immediately to avoid
 // the having the VMM blocked on writes to the pipe. To achieve this one thread
 // takes care of (and only of) all read calls (from console output and from the
@@ -46,22 +49,76 @@ DEFINE_int32(console_out_fd,
 // protected by a mutex.
 class ConsoleForwarder {
  public:
-  ConsoleForwarder(cuttlefish::SharedFD socket,
+  ConsoleForwarder(std::string console_path,
                    cuttlefish::SharedFD console_in,
                    cuttlefish::SharedFD console_out,
-                   cuttlefish::SharedFD console_log) : socket_(socket),
+                   cuttlefish::SharedFD console_log) :
+                                                console_path_(console_path),
                                                 console_in_(console_in),
                                                 console_out_(console_out),
                                                 console_log_(console_log) {}
   [[noreturn]] void StartServer() {
-    // Create a new thread to handle writes to the console and to the any client
-    // connected to the socket.
+    // Create a new thread to handle writes to the console
     writer_thread_ = std::thread([this]() { WriteLoop(); });
     // Use the calling thread (likely the process' main thread) to handle
     // reading the console's output and input from the client.
     ReadLoop();
   }
  private:
+
+  cuttlefish::SharedFD OpenPTY() {
+    // Remove any stale symlink to a pts device
+    auto ret = unlink(console_path_.c_str());
+    if (ret < 0 && errno != ENOENT) {
+      LOG(ERROR) << "Failed to unlink " << console_path_.c_str()
+                 << ": " << strerror(errno);
+      std::exit(-5);
+    }
+
+    auto pty = posix_openpt(O_RDWR | O_NOCTTY);
+    if (pty < 0) {
+      LOG(ERROR) << "Failed to open a PTY: " << strerror(errno);
+      std::exit(-6);
+    }
+    grantpt(pty);
+    unlockpt(pty);
+
+    // Disable all echo modes on the PTY
+    struct termios termios;
+    if (tcgetattr(pty, &termios) < 0) {
+      LOG(ERROR) << "Failed to get terminal control: " << strerror(errno);
+      std::exit(-7);
+    }
+    termios.c_lflag &= ~(ECHO | ECHOE | ECHOK | ECHONL);
+    termios.c_oflag &= ~(ONLCR);
+    if (tcsetattr(pty, TCSANOW, &termios) < 0) {
+      LOG(ERROR) << "Failed to set terminal control: " << strerror(errno);
+      std::exit(-8);
+    }
+
+    auto pty_dev_name = ptsname(pty);
+    if (pty_dev_name == nullptr) {
+      LOG(ERROR) << "Failed to obtain PTY device name: " << strerror(errno);
+      std::exit(-9);
+    }
+
+    if (symlink(pty_dev_name, console_path_.c_str()) < 0) {
+      LOG(ERROR) << "Failed to create symlink to " << pty_dev_name << " at "
+                 << console_path_.c_str() << ": " << strerror(errno);
+      std::exit(-10);
+    }
+
+    auto pty_shared_fd = cuttlefish::SharedFD::Dup(pty);
+    close(pty);
+    if (!pty_shared_fd->IsOpen()) {
+      LOG(ERROR) << "Error dupping fd " << pty << ": "
+                 << pty_shared_fd->StrError();
+      std::exit(-11);
+    }
+
+    return pty_shared_fd;
+  }
+
   void EnqueueWrite(std::shared_ptr<std::vector<char>> buf_ptr, cuttlefish::SharedFD fd) {
     std::lock_guard<std::mutex> lock(write_queue_mutex_);
     write_queue_.emplace_back(fd, buf_ptr);
@@ -111,13 +168,14 @@ class ConsoleForwarder {
   [[noreturn]] void ReadLoop() {
     cuttlefish::SharedFD client_fd;
     while (true) {
-      cuttlefish::SharedFDSet read_set;
-      if (client_fd->IsOpen()) {
-        read_set.Set(client_fd);
-      } else {
-        read_set.Set(socket_);
+      if (!client_fd->IsOpen()) {
+        client_fd = OpenPTY();
       }
+
+      cuttlefish::SharedFDSet read_set;
       read_set.Set(console_out_);
+      read_set.Set(client_fd);
+
       cuttlefish::Select(&read_set, nullptr, nullptr, nullptr);
       if (read_set.IsSet(console_out_)) {
         std::shared_ptr<std::vector<char>> buf_ptr = std::make_shared<std::vector<char>>(4096);
@@ -126,7 +184,7 @@ class ConsoleForwarder {
           LOG(ERROR) << "Error reading from console output: "
                      << console_out_->StrError();
           // This is likely unrecoverable, so exit here
-          std::exit(-4);
+          std::exit(-12);
         }
         buf_ptr->resize(bytes_read);
         EnqueueWrite(buf_ptr, console_log_);
@@ -134,23 +192,16 @@ class ConsoleForwarder {
           EnqueueWrite(buf_ptr, client_fd);
         }
       }
-      if (read_set.IsSet(socket_)) {
-        // socket_ will only be included in the select call (and therefore only
-        // present in the read set) if there is no client connected, so this
-        // assignment is safe.
-        client_fd = cuttlefish::SharedFD::Accept(*socket_);
-        if (!client_fd->IsOpen()) {
-          LOG(ERROR) << "Error accepting connection on socket: "
-                     << client_fd->StrError();
-        }
-      }
       if (read_set.IsSet(client_fd)) {
         std::shared_ptr<std::vector<char>> buf_ptr = std::make_shared<std::vector<char>>(4096);
         auto bytes_read = client_fd->Read(buf_ptr->data(), buf_ptr->size());
         if (bytes_read <= 0) {
+          // If this happens, it's usually because the PTY controller went away
+          // e.g. the user closed minicom, or killed screen, or closed kgdb. In
+          // such a case, we will just re-create the PTY
           LOG(ERROR) << "Error reading from client fd: "
                      << client_fd->StrError();
-          client_fd->Close(); // ignore errors here
+          client_fd->Close();
         } else {
           buf_ptr->resize(bytes_read);
           EnqueueWrite(buf_ptr, console_in_);
@@ -159,7 +210,7 @@ class ConsoleForwarder {
     }
   }
 
-  cuttlefish::SharedFD socket_;
+  std::string console_path_;
   cuttlefish::SharedFD console_in_;
   cuttlefish::SharedFD console_out_;
   cuttlefish::SharedFD console_log_;
@@ -193,35 +244,25 @@ int main(int argc, char** argv) {
   if (!console_out->IsOpen()) {
     LOG(ERROR) << "Error dupping fd " << FLAGS_console_out_fd << ": "
                << console_out->StrError();
-    return -2;
+    return -3;
   }
 
   auto config = cuttlefish::CuttlefishConfig::Get();
   if (!config) {
     LOG(ERROR) << "Unable to get config object";
-    return -3;
+    return -4;
   }
 
   auto instance = config->ForDefaultInstance();
-  auto console_socket_name = instance.console_path();
-  auto socket = cuttlefish::SharedFD::SocketLocalServer(console_socket_name.c_str(),
-                                                 false,
-                                                 SOCK_STREAM,
-                                                 0600);
-  if (!socket->IsOpen()) {
-    LOG(ERROR) << "Failed to create console socket at " << console_socket_name
-               << ": " << socket->StrError();
-    return -5;
-  }
-
+  auto console_path = instance.console_path();
   auto console_log = instance.PerInstancePath("console_log");
   auto console_log_fd = cuttlefish::SharedFD::Open(console_log.c_str(), O_CREAT | O_APPEND | O_WRONLY, 0666);
-  ConsoleForwarder console_forwarder(socket, console_in, console_out, console_log_fd);
+  ConsoleForwarder console_forwarder(console_path, console_in, console_out, console_log_fd);
 
   // Don't get a SIGPIPE from the clients
   if (sigaction(SIGPIPE, nullptr, nullptr) != 0) {
     LOG(FATAL) << "Failed to set SIGPIPE to be ignored: " << strerror(errno);
-    return -6;
+    return -13;
   }
 
   console_forwarder.StartServer();

msft-mirror-aosp/device.google.cuttlefish

(cherry picked from commit 6d2a6849d86170785de3479e7a1a51b18aa5f9b0)

host/commands/console_forwarder/main.cpp
@@ -14,7 +14,10 @@
  * limitations under the License.
  */
 
+#include <termios.h>
+#include <stdlib.h>
 #include <signal.h>
+#include <unistd.h>
 
 #include <deque>
 #include <thread>
@@ -34,9 +37,9 @@ DEFINE_int32(console_out_fd,
              -1,
              "File descriptor for the console's output channel");
 
-// Handles forwarding the serial console to a socket.
-// It receives the socket fd along with a couple of fds for the console (could
-// be the same fd twice if, for example a socket_pair were used).
+// Handles forwarding the serial console to a pseudo-terminal (PTY)
+// It receives a couple of fds for the console (could be the same fd twice if,
+// for example a socket_pair were used).
 // Data available in the console's output needs to be read immediately to avoid
 // the having the VMM blocked on writes to the pipe. To achieve this one thread
 // takes care of (and only of) all read calls (from console output and from the
@@ -45,20 +48,73 @@ DEFINE_int32(console_out_fd,
 // protected by a mutex.
 class ConsoleForwarder {
  public:
-  ConsoleForwarder(cvd::SharedFD socket,
+  ConsoleForwarder(std::string console_path,
                    cvd::SharedFD console_in,
-                   cvd::SharedFD console_out) : socket_(socket),
+                   cvd::SharedFD console_out) :
+                                                console_path_(console_path),
                                                 console_in_(console_in),
                                                 console_out_(console_out) {}
   [[noreturn]] void StartServer() {
-    // Create a new thread to handle writes to the console and to the any client
-    // connected to the socket.
+    // Create a new thread to handle writes to the console
     writer_thread_ = std::thread([this]() { WriteLoop(); });
     // Use the calling thread (likely the process' main thread) to handle
     // reading the console's output and input from the client.
     ReadLoop();
   }
  private:
+  cvd::SharedFD OpenPTY() {
+    // Remove any stale symlink to a pts device
+    auto ret = unlink(console_path_.c_str());
+    if (ret < 0 && errno != ENOENT) {
+      LOG(ERROR) << "Failed to unlink " << console_path_.c_str()
+                 << ": " << strerror(errno);
+      std::exit(-5);
+    }
+
+    auto pty = posix_openpt(O_RDWR | O_NOCTTY);
+    if (pty < 0) {
+      LOG(ERROR) << "Failed to open a PTY: " << strerror(errno);
+      std::exit(-6);
+    }
+    grantpt(pty);
+    unlockpt(pty);
+
+    // Disable all echo modes on the PTY
+    struct termios termios;
+    if (tcgetattr(pty, &termios) < 0) {
+      LOG(ERROR) << "Failed to get terminal control: " << strerror(errno);
+      std::exit(-7);
+    }
+    termios.c_lflag &= ~(ECHO | ECHOE | ECHOK | ECHONL);
+    termios.c_oflag &= ~(ONLCR);
+    if (tcsetattr(pty, TCSANOW, &termios) < 0) {
+      LOG(ERROR) << "Failed to set terminal control: " << strerror(errno);
+      std::exit(-8);
+    }
+
+    auto pty_dev_name = ptsname(pty);
+    if (pty_dev_name == nullptr) {
+      LOG(ERROR) << "Failed to obtain PTY device name: " << strerror(errno);
+      std::exit(-9);
+    }
+
+    if (symlink(pty_dev_name, console_path_.c_str()) < 0) {
+      LOG(ERROR) << "Failed to create symlink to " << pty_dev_name << " at "
+                 << console_path_.c_str() << ": " << strerror(errno);
+      std::exit(-10);
+    }
+
+    auto pty_shared_fd = cvd::SharedFD::Dup(pty);
+    close(pty);
+    if (!pty_shared_fd->IsOpen()) {
+      LOG(ERROR) << "Error dupping fd " << pty << ": "
+                 << pty_shared_fd->StrError();
+      std::exit(-11);
+    }
+
+    return pty_shared_fd;
+  }
+
   void EnqueueWrite(std::vector<char> buffer, cvd::SharedFD fd) {
     std::lock_guard<std::mutex> lock(write_queue_mutex_);
     write_queue_.emplace_back(fd, std::move(buffer));
@@ -108,13 +164,14 @@ class ConsoleForwarder {
   [[noreturn]] void ReadLoop() {
     cvd::SharedFD client_fd;
     while (true) {
-      cvd::SharedFDSet read_set;
-      if (client_fd->IsOpen()) {
-        read_set.Set(client_fd);
-      } else {
-        read_set.Set(socket_);
+      if (!client_fd->IsOpen()) {
+        client_fd = OpenPTY();
       }
+
+      cvd::SharedFDSet read_set;
       read_set.Set(console_out_);
+      read_set.Set(client_fd);
+
       cvd::Select(&read_set, nullptr, nullptr, nullptr);
       if (read_set.IsSet(console_out_)) {
         std::vector<char> buffer(4096);
@@ -123,30 +180,23 @@ class ConsoleForwarder {
           LOG(ERROR) << "Error reading from console output: "
                      << console_out_->StrError();
           // This is likely unrecoverable, so exit here
-          std::exit(-4);
+          std::exit(-12);
         }
         buffer.resize(bytes_read);
         if (client_fd->IsOpen()) {
           EnqueueWrite(std::move(buffer), client_fd);
         }
       }
-      if (read_set.IsSet(socket_)) {
-        // socket_ will only be included in the select call (and therefore only
-        // present in the read set) if there is no client connected, so this
-        // assignment is safe.
-        client_fd = cvd::SharedFD::Accept(*socket_);
-        if (!client_fd->IsOpen()) {
-          LOG(ERROR) << "Error accepting connection on socket: "
-                     << client_fd->StrError();
-        }
-      }
       if (read_set.IsSet(client_fd)) {
         std::vector<char> buffer(4096);
         auto bytes_read = client_fd->Read(buffer.data(), buffer.size());
         if (bytes_read <= 0) {
+          // If this happens, it's usually because the PTY controller went away
+          // e.g. the user closed minicom, or killed screen, or closed kgdb. In
+          // such a case, we will just re-create the PTY
           LOG(ERROR) << "Error reading from client fd: "
                      << client_fd->StrError();
-          client_fd->Close(); // ignore errors here
+          client_fd->Close();
         } else {
           buffer.resize(bytes_read);
           EnqueueWrite(std::move(buffer), console_in_);
@@ -155,7 +205,7 @@ class ConsoleForwarder {
     }
   }
 
-  cvd::SharedFD socket_;
+  std::string console_path_;
   cvd::SharedFD console_in_;
   cvd::SharedFD console_out_;
   std::thread writer_thread_;
@@ -188,33 +238,23 @@ int main(int argc, char** argv) {
   if (!console_out->IsOpen()) {
     LOG(ERROR) << "Error dupping fd " << FLAGS_console_out_fd << ": "
                << console_out->StrError();
-    return -2;
+    return -3;
   }
 
   auto config = vsoc::CuttlefishConfig::Get();
   if (!config) {
     LOG(ERROR) << "Unable to get config object";
-    return -3;
+    return -4;
   }
 
   auto instance = config->ForDefaultInstance();
-  auto console_socket_name = instance.console_path();
-  auto socket = cvd::SharedFD::SocketLocalServer(console_socket_name.c_str(),
-                                                 false,
-                                                 SOCK_STREAM,
-                                                 0600);
-  if (!socket->IsOpen()) {
-    LOG(ERROR) << "Failed to create console socket at " << console_socket_name
-               << ": " << socket->StrError();
-    return -5;
-  }
-
-  ConsoleForwarder console_forwarder(socket, console_in, console_out);
+  auto console_path = instance.console_path();
+  ConsoleForwarder console_forwarder(console_path, console_in, console_out);
 
   // Don't get a SIGPIPE from the clients
   if (sigaction(SIGPIPE, nullptr, nullptr) != 0) {
     LOG(FATAL) << "Failed to set SIGPIPE to be ignored: " << strerror(errno);
-    return -6;
+    return -13;
   }
 
   console_forwarder.StartServer();