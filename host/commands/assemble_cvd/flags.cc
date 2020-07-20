google/android-cuttlefish

Path $HOME/bin for extract-vmlinux
The extract-vmlinux script may call out to decompression tools the host
does not have installed, like lz4. Ensure that the system and user bin
paths, as well as the artifact bin path, are in PATH for
extract-vmlinux.

Bug: 160705218
Change-Id: I004ed056f37706722305561e032f4bcbfb1a71d0

host/commands/assemble_cvd/flags.cc
@@ -784,6 +784,9 @@ bool CleanPriorFiles(const cuttlefish::CuttlefishConfig& config, const std::set<
 bool DecompressKernel(const std::string& src, const std::string& dst) {
   cuttlefish::Command decomp_cmd(cuttlefish::DefaultHostArtifactsPath("bin/extract-vmlinux"));
   decomp_cmd.AddParameter(src);
+  std::string current_path = getenv("PATH") == nullptr ? "" : getenv("PATH");
+  std::string bin_folder = cuttlefish::DefaultHostArtifactsPath("bin");
+  decomp_cmd.SetEnvironment({"PATH=" + current_path + ":" + bin_folder});
   auto output_file = cuttlefish::SharedFD::Creat(dst.c_str(), 0666);
   if (!output_file->IsOpen()) {
     LOG(ERROR) << "Unable to create decompressed image file: "

msft-mirror-aosp/device.google.cuttlefish

(cherry picked from commit 4eb779dd0060b75b2680293e989539950c566c23)

host/commands/assemble_cvd/flags.cc
@@ -648,6 +648,9 @@ bool CleanPriorFiles(const vsoc::CuttlefishConfig& config, const std::set<std::s
 bool DecompressKernel(const std::string& src, const std::string& dst) {
   cvd::Command decomp_cmd(vsoc::DefaultHostArtifactsPath("bin/extract-vmlinux"));
   decomp_cmd.AddParameter(src);
+  std::string current_path = getenv("PATH") == nullptr ? "" : getenv("PATH");
+  std::string bin_folder = vsoc::DefaultHostArtifactsPath("bin");
+  decomp_cmd.SetEnvironment({"PATH=" + current_path + ":" + bin_folder});
   auto output_file = cvd::SharedFD::Creat(dst.c_str(), 0666);
   if (!output_file->IsOpen()) {
     LOG(ERROR) << "Unable to create decompressed image file: "