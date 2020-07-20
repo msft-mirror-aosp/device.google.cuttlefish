google/android-cuttlefish

Remove --logcat_mode, which was broken
This feature used to work but now nothing "listens" on the serial port
set up in the qemu backend, and crosvm has no support for the feature.

Bug: 160341724
Change-Id: Ia6e2ee6303442f3cd38d7ec3ffe6d575beca836c

host/commands/run_cvd/launch.h
@@ -29,8 +29,8 @@ StreamerLaunchResult LaunchVNCServer(
 
 void LaunchTombstoneReceiverIfEnabled(const cuttlefish::CuttlefishConfig& config,
                                       cuttlefish::ProcessMonitor* process_monitor);
-void LaunchLogcatReceiverIfEnabled(const cuttlefish::CuttlefishConfig& config,
-                                   cuttlefish::ProcessMonitor* process_monitor);
+void LaunchLogcatReceiver(const cuttlefish::CuttlefishConfig& config,
+                          cuttlefish::ProcessMonitor* process_monitor);
 void LaunchConfigServer(const cuttlefish::CuttlefishConfig& config,
                         cuttlefish::ProcessMonitor* process_monitor);
 

msft-mirror-aosp/device.google.cuttlefish

(cherry picked from commit 261416a5d0d05e6e23a2fe34b9f07d7474a8e152)

host/commands/run_cvd/launch.h
@@ -45,8 +45,8 @@ ConfigServerPorts LaunchConfigServer(const vsoc::CuttlefishConfig& config,
 struct LogcatServerPorts {
   std::optional<unsigned int> server_vsock_port;
 };
-LogcatServerPorts LaunchLogcatReceiverIfEnabled(const vsoc::CuttlefishConfig& config,
-                                                cvd::ProcessMonitor* process_monitor);
+LogcatServerPorts LaunchLogcatReceiver(const vsoc::CuttlefishConfig& config,
+                                       cvd::ProcessMonitor* process_monitor);
 
 StreamerLaunchResult LaunchWebRTC(cvd::ProcessMonitor* process_monitor,
                                   const vsoc::CuttlefishConfig& config);