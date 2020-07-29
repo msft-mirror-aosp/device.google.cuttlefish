google/android-cuttlefish

Replace cf_qemu.sh with a direct invocation.
This has more parity with crosvm_manager.cpp.

Test: launch_cvd -vm_manager=qemu_cli
Change-Id: Ibb91988cd61a6604334c37569c09219ef3e77fd3

host/libs/vm_manager/qemu_manager.cpp
@@ -53,21 +53,6 @@ void LogAndSetEnv(const char* key, const std::string& value) {
   LOG(INFO) << key << "=" << value;
 }
 
-std::string JoinString(const std::vector<std::string>& args,
-                       const std::string& delim) {
-  bool first = true;
-  std::stringstream output;
-  for (const auto& arg : args) {
-    if (first) {
-      first = false;
-    } else {
-      output << delim;
-    }
-    output << arg;
-  }
-  return output.str();
-}
-
 bool Stop() {
   auto config = vsoc::CuttlefishConfig::Get();
   auto monitor_path = GetMonitorPath(config);
@@ -129,39 +114,160 @@ QemuManager::QemuManager(const vsoc::CuttlefishConfig* config)
 
 std::vector<cvd::Command> QemuManager::StartCommands() {
   auto instance = config_->ForDefaultInstance();
-  // Set the config values in the environment
-  LogAndSetEnv("qemu_binary", config_->qemu_binary());
-  LogAndSetEnv("instance_name", instance.instance_name());
-  LogAndSetEnv("memory_mb", std::to_string(config_->memory_mb()));
-  LogAndSetEnv("cpus", std::to_string(config_->cpus()));
-  LogAndSetEnv("uuid", instance.uuid());
-  LogAndSetEnv("monitor_path", GetMonitorPath(config_));
-  LogAndSetEnv("kernel_image_path", config_->GetKernelImageToUse());
-  LogAndSetEnv("gdb_flag", config_->gdb_flag());
-  LogAndSetEnv("ramdisk_image_path", config_->final_ramdisk_path());
-  LogAndSetEnv("kernel_cmdline", kernel_cmdline_);
-  LogAndSetEnv("virtual_disk_paths", JoinString(instance.virtual_disk_paths(),
-                                                ";"));
-  LogAndSetEnv("wifi_tap_name", instance.wifi_tap_name());
-  LogAndSetEnv("mobile_tap_name", instance.mobile_tap_name());
-  LogAndSetEnv("kernel_log_pipe_name", instance.kernel_log_pipe_name());
-  LogAndSetEnv("console_path", instance.console_path());
-  LogAndSetEnv("logcat_path", instance.logcat_path());
-  LogAndSetEnv("vsock_guest_cid", std::to_string(instance.vsock_guest_cid()));
-  LogAndSetEnv("logcat_mode", config_->logcat_mode());
-  LogAndSetEnv("use_bootloader", config_->use_bootloader() ? "true" : "false");
-  LogAndSetEnv("bootloader", config_->bootloader());
 
-  cvd::Command qemu_cmd(vsoc::DefaultHostArtifactsPath("bin/cf_qemu.sh"),
-                        [](cvd::Subprocess* proc) {
-                          auto stopped = Stop();
-                          if (stopped) {
-                            return true;
-                          }
-                          LOG(WARNING) << "Failed to stop VMM nicely, "
-                                       << "attempting to KILL";
-                          return KillSubprocess(proc);
-                        });
+  auto stop = [](cvd::Subprocess* proc) {
+    auto stopped = Stop();
+    if (stopped) {
+      return true;
+    }
+    LOG(WARNING) << "Failed to stop VMM nicely, "
+                  << "attempting to KILL";
+    return KillSubprocess(proc);
+  };
+
+  bool is_arm = android::base::EndsWith(config_->qemu_binary(), "system-aarch64");
+
+  cvd::Command qemu_cmd(config_->qemu_binary(), stop);
+  qemu_cmd.AddParameter("-name");
+  qemu_cmd.AddParameter("guest=", instance.instance_name(), ",debug-threads=on");
+
+  qemu_cmd.AddParameter("-machine");
+  auto machine = is_arm ? "virt,gic_version=2" : "pc-i440fx-2.8,accel=kvm";
+  qemu_cmd.AddParameter(machine, ",usb=off,dump-guest-core=off");
+
+  qemu_cmd.AddParameter("-m");
+  qemu_cmd.AddParameter(config_->memory_mb());
+
+  qemu_cmd.AddParameter("-realtime");
+  qemu_cmd.AddParameter("mlock=off");
+
+  qemu_cmd.AddParameter("-smp");
+  qemu_cmd.AddParameter(config_->cpus(), ",sockets=", config_->cpus(),
+                        ",cores=1,threads=1");
+
+  qemu_cmd.AddParameter("-uuid");
+  qemu_cmd.AddParameter(instance.uuid());
+
+  qemu_cmd.AddParameter("-display");
+  qemu_cmd.AddParameter("none");
+
+  qemu_cmd.AddParameter("-no-user-config");
+  qemu_cmd.AddParameter("-nodefaults");
+  qemu_cmd.AddParameter("-no-shutdown");
+
+  qemu_cmd.AddParameter("-rtc");
+  qemu_cmd.AddParameter("base=utc");
+
+  qemu_cmd.AddParameter("-boot");
+  qemu_cmd.AddParameter("strict=on");
+
+  qemu_cmd.AddParameter("-kernel");
+  qemu_cmd.AddParameter(config_->GetKernelImageToUse());
+
+  qemu_cmd.AddParameter("-append");
+  qemu_cmd.AddParameter(kernel_cmdline_);
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtio-serial-pci,id=virtio-serial0");
+
+  for (size_t i = 0; i < instance.virtual_disk_paths().size(); i++) {
+    auto bootindex = i == 0 ? ",bootindex=1" : "";
+    auto disk = instance.virtual_disk_paths()[i];
+    qemu_cmd.AddParameter("-drive");
+    qemu_cmd.AddParameter("file=", disk, ",if=none,id=drive-virtio-disk", i,
+                          ",aio=threads");
+    qemu_cmd.AddParameter("-device");
+    qemu_cmd.AddParameter("virtio-blk-pci,scsi=off,drive=drive-virtio-disk", i,
+                          ",id=virtio-disk", i, bootindex);
+  }
+
+  qemu_cmd.AddParameter("-netdev");
+  qemu_cmd.AddParameter("tap,id=hostnet0,ifname=", instance.wifi_tap_name(),
+                        ",script=no,downscript=no");
+
+  auto romfile = is_arm ? ",romfile" : "";
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtio-net-pci,netdev=hostnet0,id=net0", romfile);
+
+  qemu_cmd.AddParameter("-netdev");
+  qemu_cmd.AddParameter("tap,id=hostnet1,ifname=", instance.mobile_tap_name(),
+                        ",script=no,downscript=no");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtio-net-pci,netdev=hostnet1,id=net1", romfile);
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtio-balloon-pci,id=balloon0");
+
+  qemu_cmd.AddParameter("-object");
+  qemu_cmd.AddParameter("rng-random,id=objrng0,filename=/dev/urandom");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtio-rng-pci,rng=objrng0,id=rng0,",
+                        "max-bytes=1024,period=2000");
+
+  qemu_cmd.AddParameter("-cpu");
+  qemu_cmd.AddParameter(is_arm ? "cortex-a53" : "host");
+
+  qemu_cmd.AddParameter("-msg");
+  qemu_cmd.AddParameter("timestamp=on");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("AC97");
+
+  if (config_->use_bootloader()) {
+    qemu_cmd.AddParameter("-bios");
+    qemu_cmd.AddParameter(config_->bootloader());
+  }
+
+  qemu_cmd.AddParameter("-chardev");
+  qemu_cmd.AddParameter("socket,id=charmonitor,path=", GetMonitorPath(config_),
+                        ",server,nowait");
+
+  qemu_cmd.AddParameter("-mon");
+  qemu_cmd.AddParameter("chardev=charmonitor,id=monitor,mode=control");
+
+  qemu_cmd.AddParameter("-chardev");
+  qemu_cmd.AddParameter("file,id=charserial0,path=",
+                        instance.kernel_log_pipe_name(), ",append=on");
+
+  qemu_cmd.AddParameter("-device");
+  // On ARM, the early console can be PCI, and ISA is not supported
+  // On x86, the early console must be ISA, not PCI, so we start to get kernel
+  // messages as soon as possible. ISA devices do not have 'addr' assignments.
+  auto kernel_console_serial = is_arm ? "pci-serial" : "isa-serial";
+  qemu_cmd.AddParameter(kernel_console_serial, ",chardev=charserial0,id=serial0");
+
+  qemu_cmd.AddParameter("-chardev");
+  qemu_cmd.AddParameter("socket,id=charserial1,path=", instance.console_path(),
+                        ",server,nowait");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter(kernel_console_serial, ",chardev=charserial1,id=serial1");
+
+  if (config_->logcat_mode() == "serial") {
+    qemu_cmd.AddParameter("-chardev");
+    qemu_cmd.AddParameter("file,id=charchannel0,path=", instance.logcat_path(),
+                          ",append=on");
+
+    qemu_cmd.AddParameter("-device");
+    qemu_cmd.AddParameter("virtserialport,bus=virtio-serial0.0,nr=1,",
+                          "chardev=charchannel0,id=channel0,name=cf-logcat");
+  }
+
+  if (config_->gdb_flag().size() > 0) {
+    qemu_cmd.AddParameter("-gdb");
+    qemu_cmd.AddParameter(config_->gdb_flag());
+  }
+
+  qemu_cmd.AddParameter("-initrd");
+  qemu_cmd.AddParameter(config_->final_ramdisk_path());
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("vhost-vsock-pci,guest-cid=", instance.vsock_guest_cid());
+
+  LogAndSetEnv("QEMU_AUDIO_DRV", "none");
+
   std::vector<cvd::Command> ret;
   ret.push_back(std::move(qemu_cmd));
   return ret;

msft-mirror-aosp/device.google.cuttlefish

host/libs/vm_manager/qemu_manager.cpp
@@ -52,21 +52,6 @@ void LogAndSetEnv(const char* key, const std::string& value) {
   LOG(INFO) << key << "=" << value;
 }
 
-std::string JoinString(const std::vector<std::string>& args,
-                       const std::string& delim) {
-  bool first = true;
-  std::stringstream output;
-  for (const auto& arg : args) {
-    if (first) {
-      first = false;
-    } else {
-      output << delim;
-    }
-    output << arg;
-  }
-  return output.str();
-}
-
 bool Stop() {
   auto config = cuttlefish::CuttlefishConfig::Get();
   auto monitor_path = GetMonitorPath(config);
@@ -125,39 +110,169 @@ QemuManager::QemuManager(const cuttlefish::CuttlefishConfig* config)
   : VmManager(config) {}
 
 std::vector<cuttlefish::Command> QemuManager::StartCommands() {
-  // Set the config values in the environment
-  LogAndSetEnv("qemu_binary", config_->qemu_binary());
-  LogAndSetEnv("instance_name", config_->instance_name());
-  LogAndSetEnv("memory_mb", std::to_string(config_->memory_mb()));
-  LogAndSetEnv("cpus", std::to_string(config_->cpus()));
-  LogAndSetEnv("uuid", config_->uuid());
-  LogAndSetEnv("monitor_path", GetMonitorPath(config_));
-  LogAndSetEnv("kernel_image_path", config_->GetKernelImageToUse());
-  LogAndSetEnv("gdb_flag", config_->gdb_flag());
-  LogAndSetEnv("ramdisk_image_path", config_->final_ramdisk_path());
-  LogAndSetEnv("kernel_cmdline", kernel_cmdline_);
-  LogAndSetEnv("virtual_disk_paths", JoinString(config_->virtual_disk_paths(),
-                                                ";"));
-  LogAndSetEnv("wifi_tap_name", config_->wifi_tap_name());
-  LogAndSetEnv("mobile_tap_name", config_->mobile_tap_name());
-  LogAndSetEnv("kernel_log_pipe_name",
-               config_->kernel_log_pipe_name());
-  LogAndSetEnv("console_path", config_->console_path());
-  LogAndSetEnv("logcat_path", config_->logcat_path());
-  LogAndSetEnv("vsock_guest_cid", std::to_string(config_->vsock_guest_cid()));
-  LogAndSetEnv("use_bootloader", config_->use_bootloader() ? "true" : "false");
-  LogAndSetEnv("bootloader", config_->bootloader());
+  auto stop = [](cuttlefish::Subprocess* proc) {
+    auto stopped = Stop();
+    if (stopped) {
+      return true;
+    }
+    LOG(WARNING) << "Failed to stop VMM nicely, "
+                  << "attempting to KILL";
+    return KillSubprocess(proc);
+  };
 
-  cuttlefish::Command qemu_cmd(cuttlefish::DefaultHostArtifactsPath("bin/cf_qemu.sh"),
-                        [](cuttlefish::Subprocess* proc) {
-                          auto stopped = Stop();
-                          if (stopped) {
-                            return true;
-                          }
-                          LOG(WARNING) << "Failed to stop VMM nicely, "
-                                       << "attempting to KILL";
-                          return KillSubprocess(proc);
-                        });
+  bool is_arm = android::base::EndsWith(config_->qemu_binary(),
+                                        "system-aarch64");
+
+  cuttlefish::Command qemu_cmd(config_->qemu_binary(), stop);
+  qemu_cmd.AddParameter("-name");
+  qemu_cmd.AddParameter("guest=", config_->instance_name(),
+                        ",debug-threads=on");
+
+  qemu_cmd.AddParameter("-machine");
+  auto machine = is_arm ? "virt,gic_version=2" : "pc-i440fx-2.8,accel=kvm";
+  qemu_cmd.AddParameter(machine, ",usb=off,dump-guest-core=off");
+
+  qemu_cmd.AddParameter("-m");
+  qemu_cmd.AddParameter(config_->memory_mb());
+
+  qemu_cmd.AddParameter("-overcommit");
+  qemu_cmd.AddParameter("mem-lock=off");
+
+  qemu_cmd.AddParameter("-smp");
+  qemu_cmd.AddParameter(config_->cpus(), ",sockets=", config_->cpus(),
+                        ",cores=1,threads=1");
+
+  qemu_cmd.AddParameter("-uuid");
+  qemu_cmd.AddParameter(config_->uuid());
+
+  qemu_cmd.AddParameter("-display");
+  qemu_cmd.AddParameter("none");
+
+  qemu_cmd.AddParameter("-no-user-config");
+  qemu_cmd.AddParameter("-nodefaults");
+
+  qemu_cmd.AddParameter("-rtc");
+  qemu_cmd.AddParameter("base=utc");
+
+  qemu_cmd.AddParameter("-no-shutdown");
+
+  qemu_cmd.AddParameter("-boot");
+  qemu_cmd.AddParameter("strict=on");
+
+  qemu_cmd.AddParameter("-kernel");
+  qemu_cmd.AddParameter(config_->GetKernelImageToUse());
+
+  qemu_cmd.AddParameter("-append");
+  qemu_cmd.AddParameter(kernel_cmdline_);
+
+  qemu_cmd.AddParameter("-chardev");
+  qemu_cmd.AddParameter("socket,id=charmonitor,path=", GetMonitorPath(config_),
+                        ",server,nowait");
+
+  qemu_cmd.AddParameter("-mon");
+  qemu_cmd.AddParameter("chardev=charmonitor,id=monitor,mode=control");
+
+  qemu_cmd.AddParameter("-chardev");
+  qemu_cmd.AddParameter("file,id=earlycon,path=",
+                        config_->kernel_log_pipe_name(), ",append=on");
+
+  qemu_cmd.AddParameter("-serial");
+  qemu_cmd.AddParameter("chardev:earlycon");
+
+  qemu_cmd.AddParameter("-chardev");
+  qemu_cmd.AddParameter("file,id=hvc0,path=",
+                        config_->kernel_log_pipe_name(), ",append=on");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtio-serial-pci,max_ports=1,id=virtio-serial0");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtconsole,bus=virtio-serial0.0,chardev=hvc0");
+
+  qemu_cmd.AddParameter("-chardev");
+  qemu_cmd.AddParameter("socket,id=hvc1,path=", config_->console_path(),
+                        ",server,nowait");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtio-serial-pci,max_ports=1,id=virtio-serial1");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtconsole,bus=virtio-serial1.0,chardev=hvc1");
+
+  qemu_cmd.AddParameter("-chardev");
+  qemu_cmd.AddParameter("file,id=hvc2,path=",
+                        config_->logcat_pipe_name(), ",append=on");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtio-serial-pci,max_ports=1,id=virtio-serial2");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtconsole,bus=virtio-serial2.0,chardev=hvc2");
+
+  for (size_t i = 0; i < config_->virtual_disk_paths().size(); i++) {
+    auto bootindex = i == 0 ? ",bootindex=1" : "";
+    auto disk = config_->virtual_disk_paths()[i];
+    qemu_cmd.AddParameter("-drive");
+    qemu_cmd.AddParameter("file=", disk, ",if=none,id=drive-virtio-disk", i,
+                          ",aio=threads,format=raw");
+    qemu_cmd.AddParameter("-device");
+    qemu_cmd.AddParameter("virtio-blk-pci,scsi=off,drive=drive-virtio-disk", i,
+                          ",id=virtio-disk", i, bootindex);
+  }
+
+  qemu_cmd.AddParameter("-netdev");
+  qemu_cmd.AddParameter("tap,id=hostnet0,ifname=", config_->wifi_tap_name(),
+                        ",script=no,downscript=no");
+
+  auto romfile = is_arm ? ",romfile" : "";
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtio-net-pci,netdev=hostnet0,id=net0", romfile);
+
+  qemu_cmd.AddParameter("-netdev");
+  qemu_cmd.AddParameter("tap,id=hostnet1,ifname=", config_->mobile_tap_name(),
+                        ",script=no,downscript=no");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtio-net-pci,netdev=hostnet1,id=net1", romfile);
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtio-balloon-pci,id=balloon0");
+
+  qemu_cmd.AddParameter("-object");
+  qemu_cmd.AddParameter("rng-random,id=objrng0,filename=/dev/urandom");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("virtio-rng-pci,rng=objrng0,id=rng0,",
+                        "max-bytes=1024,period=2000");
+
+  qemu_cmd.AddParameter("-cpu");
+  qemu_cmd.AddParameter(is_arm ? "cortex-a53" : "host");
+
+  qemu_cmd.AddParameter("-msg");
+  qemu_cmd.AddParameter("timestamp=on");
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("AC97");
+
+  if (config_->use_bootloader()) {
+    qemu_cmd.AddParameter("-bios");
+    qemu_cmd.AddParameter(config_->bootloader());
+  }
+
+  if (config_->gdb_flag().size() > 0) {
+    qemu_cmd.AddParameter("-gdb");
+    qemu_cmd.AddParameter(config_->gdb_flag());
+  }
+
+  qemu_cmd.AddParameter("-initrd");
+  qemu_cmd.AddParameter(config_->final_ramdisk_path());
+
+  qemu_cmd.AddParameter("-device");
+  qemu_cmd.AddParameter("vhost-vsock-pci,guest-cid=",
+                        config_->vsock_guest_cid());
+
+  LogAndSetEnv("QEMU_AUDIO_DRV", "none");
+
   std::vector<cuttlefish::Command> ret;
   ret.push_back(std::move(qemu_cmd));
   return ret;