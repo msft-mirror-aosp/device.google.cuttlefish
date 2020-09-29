google/android-cuttlefish

Implement pstore for last kmsg, mce
Our crosvm binaries already implement this feature, so it is just a
matter of setting up crosvm to use a file under cuttlefish_runtime.

This file will be powerwashed with the pmem registry and sdcard.

Bug: 159242186
Test: "adb reboot" then checked /sys/fs/pstore/ for contents
Change-Id: Id533f718edaf29aa31edebf3a77eb651df0bdde4

host/commands/assemble_cvd/flags.cc
@@ -1164,6 +1164,10 @@ const vsoc::CuttlefishConfig* InitFilesystemAndCreateConfig(
       CreateBlankImage(instance.access_kregistry_path(), 2 /* mb */, "none");
     }
 
+    if (!cvd::FileExists(instance.pstore_path())) {
+      CreateBlankImage(instance.pstore_path(), 2 /* mb */, "none");
+    }
+
     if (!cvd::FileExists(instance.sdcard_path())) {
       CreateBlankImage(instance.sdcard_path(),
                        FLAGS_blank_sdcard_image_mb, "sdcard");
@@ -1211,6 +1215,7 @@ const vsoc::CuttlefishConfig* InitFilesystemAndCreateConfig(
       }
       CreateQcowOverlay(config->crosvm_binary(), config->composite_disk_path(), overlay_path);
       CreateBlankImage(instance.access_kregistry_path(), 2 /* mb */, "none");
+      CreateBlankImage(instance.pstore_path(), 2 /* mb */, "none");
     }
   }
 

host/libs/config/cuttlefish_config.cpp
@@ -402,6 +402,10 @@ std::string CuttlefishConfig::InstanceSpecific::access_kregistry_path() const {
   return cvd::AbsolutePath(PerInstancePath("access-kregistry"));
 }
 
+std::string CuttlefishConfig::InstanceSpecific::pstore_path() const {
+  return cvd::AbsolutePath(PerInstancePath("pstore"));
+}
+
 std::string CuttlefishConfig::InstanceSpecific::console_path() const {
   return cvd::AbsolutePath(PerInstancePath("console"));
 }

host/libs/vm_manager/crosvm_manager.cpp
@@ -188,6 +188,8 @@ std::vector<cvd::Command> CrosvmManager::StartCommands() {
   AddTapFdParameter(&crosvm_cmd, instance.mobile_tap_name());
 
   crosvm_cmd.AddParameter("--rw-pmem-device=", instance.access_kregistry_path());
+  crosvm_cmd.AddParameter("--pstore=path=", instance.pstore_path(), ",size=",
+                          cvd::FileSize(instance.pstore_path()));
 
   if (config_->enable_sandbox()) {
     const bool seccomp_exists = cvd::DirectoryExists(config_->seccomp_policy_dir());

msft-mirror-aosp/device.google.cuttlefish

(cherry picked from commit 0e54040121c04a3f83174871e68d2c25a597394f)

host/commands/assemble_cvd/flags.cc
@@ -1132,6 +1132,10 @@ const cuttlefish::CuttlefishConfig* InitFilesystemAndCreateConfig(
       CreateBlankImage(instance.access_kregistry_path(), 2 /* mb */, "none");
     }
 
+    if (!cuttlefish::FileExists(instance.pstore_path())) {
+      CreateBlankImage(instance.pstore_path(), 2 /* mb */, "none");
+    }
+
     if (!cuttlefish::FileExists(instance.sdcard_path())) {
       CreateBlankImage(instance.sdcard_path(),
                        FLAGS_blank_sdcard_image_mb, "sdcard");
@@ -1174,6 +1178,7 @@ const cuttlefish::CuttlefishConfig* InitFilesystemAndCreateConfig(
       }
       CreateQcowOverlay(config->crosvm_binary(), config->composite_disk_path(), overlay_path);
       CreateBlankImage(instance.access_kregistry_path(), 2 /* mb */, "none");
+      CreateBlankImage(instance.pstore_path(), 2 /* mb */, "none");
     }
   }
 

host/libs/config/cuttlefish_config.cpp
@@ -395,6 +395,10 @@ std::string CuttlefishConfig::InstanceSpecific::access_kregistry_path() const {
   return cuttlefish::AbsolutePath(PerInstancePath("access-kregistry"));
 }
 
+std::string CuttlefishConfig::InstanceSpecific::pstore_path() const {
+  return cuttlefish::AbsolutePath(PerInstancePath("pstore"));
+}
+
 std::string CuttlefishConfig::InstanceSpecific::console_path() const {
   return cuttlefish::AbsolutePath(PerInstancePath("console"));
 }

host/libs/vm_manager/crosvm_manager.cpp
@@ -26,6 +26,7 @@
 #include <glog/logging.h>
 
 #include "common/libs/utils/environment.h"
+#include "common/libs/utils/files.h"
 #include "common/libs/utils/network.h"
 #include "common/libs/utils/subprocess.h"
 #include "host/libs/config/cuttlefish_config.h"
@@ -168,6 +169,8 @@ std::vector<cuttlefish::Command> CrosvmManager::StartCommands() {
   AddTapFdParameter(&crosvm_cmd, instance.mobile_tap_name());
 
   crosvm_cmd.AddParameter("--rw-pmem-device=", instance.access_kregistry_path());
+  crosvm_cmd.AddParameter("--pstore=path=", instance.pstore_path(), ",size=",
+                          cuttlefish::FileSize(instance.pstore_path()));
 
   // TODO remove this (use crosvm's seccomp files)
   crosvm_cmd.AddParameter("--disable-sandbox");