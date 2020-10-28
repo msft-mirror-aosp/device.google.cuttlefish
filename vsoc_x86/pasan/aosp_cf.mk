24b0cea9d52bb1d73627d5648e14c461e1177249
Alistair Delva <adelva@google.com>
2020-10-28 15:28:23 -0700

    Exclude hardware features from more AOSP targets
    Any AOSP-only target must install this permission, otherwise various *TS
    tests will fail. This was originally added only to 32-bit x86.
    
    Bug: 145127770
    Change-Id: I92540e4af515ccb3d0f5a27120192351b2a25384
    (cherry picked from commit 5a0ed1e17dc2ddb202577c18e19143cc60dff2c9)

--- vsoc_x86/pasan/aosp_cf.mk
+++ vsoc_x86/pasan/aosp_cf.mk
@@ -45,6 +45,10 @@ DEVICE_PACKAGE_OVERLAYS += device/google/cuttlefish/vsoc_x86/phone/overlay
 $(call inherit-product, device/google/cuttlefish/vsoc_x86/device.mk)
 
 
+# Exclude features that are not available on AOSP devices.
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml
+
 PRODUCT_NAME := aosp_cf_x86_pasan
 PRODUCT_DEVICE := vsoc_x86
 PRODUCT_MANUFACTURER := Google
