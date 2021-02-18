google/android-cuttlefish

Use KeymasterChannel in RemoteKeymaster.
Bug: 155328112
Test: m android.hardware.keymaster@4.0-service.remote
Change-Id: I99044f7c7fddd25b737549b0f00143adb499f996

guest/hals/keymaster/remote/remote_keymaster.cpp
@@ -16,15 +16,42 @@
 
 #include "remote_keymaster.h"
 
-#include <cutils/log.h>
+#include <android-base/logging.h>
 #include <keymaster/android_keymaster_messages.h>
 #include <keymaster/keymaster_configuration.h>
 #include "common/libs/security/keymaster_ipc.h"
 
 namespace keymaster {
 
-int RemoteKeymaster::Initialize() {
-    // TODO(schuffelen): Connect to the remote side.
+RemoteKeymaster::RemoteKeymaster(cvd::KeymasterChannel* channel)
+    : channel_(channel) {}
+
+RemoteKeymaster::~RemoteKeymaster() {
+}
+
+void RemoteKeymaster::ForwardCommand(enum keymaster_command command, const Serializable& req,
+                                     KeymasterResponse* rsp) {
+    if (!channel_->SendMessage(command, req)) {
+        LOG(ERROR) << "Failed to send keymaster message: " << command;
+        rsp->error = KM_ERROR_UNKNOWN_ERROR;
+        return;
+    }
+    auto response = channel_->ReceiveMessage();
+    if (!response) {
+        LOG(ERROR) << "Failed to receive keymaster response: " << command;
+        rsp->error = KM_ERROR_UNKNOWN_ERROR;
+        return;
+    }
+    const uint8_t* buffer = response->payload;
+    const uint8_t* buffer_end = response->payload + response->payload_size;
+    if (!rsp->Deserialize(&buffer, buffer_end)) {
+        LOG(ERROR) << "Failed to deserialize keymaster response: " << command;
+        rsp->error = KM_ERROR_UNKNOWN_ERROR;
+        return;
+    }
+}
+
+bool RemoteKeymaster::Initialize() {
 
     ConfigureRequest req;
     req.os_version = GetOsVersion();
@@ -34,30 +61,11 @@ int RemoteKeymaster::Initialize() {
     Configure(req, &rsp);
 
     if (rsp.error != KM_ERROR_OK) {
-        ALOGE("Failed to configure keymaster %d", rsp.error);
-        return -1;
+        LOG(ERROR) << "Failed to configure keymaster: " << rsp.error;
+        return false;
     }
 
-    return 0;
-}
-
-RemoteKeymaster::RemoteKeymaster() {}
-
-RemoteKeymaster::~RemoteKeymaster() {
-    // TODO(schuffelen): Disconnect from the remote side.
-}
-
-static void ForwardCommand(enum keymaster_command command, const Serializable& req,
-                           KeymasterResponse* rsp) {
-    keymaster_error_t err = KM_ERROR_OK;
-    // TODO(schuffelen): Send the message to the remote side.
-    (void) command;
-    (void) req;
-    (void) rsp;
-    if (err != KM_ERROR_OK) {
-        ALOGE("Failed to send cmd %d err: %d", command, err);
-        rsp->error = err;
-    }
+    return true;
 }
 
 void RemoteKeymaster::GetVersion(const GetVersionRequest& request, GetVersionResponse* response) {

guest/hals/keymaster/remote/remote_keymaster.h
@@ -19,13 +19,20 @@
 
 #include <keymaster/android_keymaster_messages.h>
 
+#include "common/libs/security/keymaster_channel.h"
+
 namespace keymaster {
 
 class RemoteKeymaster {
+  private:
+    cvd::KeymasterChannel* channel_;
+
+    void ForwardCommand(enum keymaster_command command, const Serializable& req,
+                        KeymasterResponse* rsp);
   public:
-    RemoteKeymaster();
+    RemoteKeymaster(cvd::KeymasterChannel*);
     ~RemoteKeymaster();
-    int Initialize();
+    bool Initialize();
     void GetVersion(const GetVersionRequest& request, GetVersionResponse* response);
     void SupportedAlgorithms(const SupportedAlgorithmsRequest& request,
                              SupportedAlgorithmsResponse* response);

guest/hals/keymaster/remote/service4.cpp
@@ -17,17 +17,35 @@
 
 #include <android-base/logging.h>
 #include <android/hardware/keymaster/4.0/IKeymasterDevice.h>
+#include <cutils/properties.h>
+#include <gflags/gflags.h>
 #include <hidl/HidlTransportSupport.h>
+
+#include "common/libs/fs/shared_fd.h"
+#include "common/libs/security/keymaster_channel.h"
 #include <guest/hals/keymaster/remote/remote_keymaster.h>
 #include <guest/hals/keymaster/remote/remote_keymaster4_device.h>
 
-int main() {
+DEFINE_uint32(
+    port,
+    static_cast<uint32_t>(property_get_int64("ro.boot.vsock_keymaster_port", 0)),
+    "virtio socket port to send keymaster commands to");
+
+int main(int argc, char** argv) {
+    ::android::base::InitLogging(argv);
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
     ::android::hardware::configureRpcThreadpool(1, true);
-    auto remoteKeymaster = new keymaster::RemoteKeymaster();
-    int err = remoteKeymaster->Initialize();
-    if (err != 0) {
-        LOG(FATAL) << "Could not initialize RemoteKeymaster (" << err << ")";
-        return -1;
+
+    auto vsockFd = cvd::SharedFD::VsockClient(2, FLAGS_port, SOCK_STREAM);
+    if (!vsockFd->IsOpen()) {
+        LOG(FATAL) << "Could not connect to keymaster server: "
+                   << vsockFd->StrError();
+    }
+    cvd::KeymasterChannel keymasterChannel(vsockFd);
+    auto remoteKeymaster = new keymaster::RemoteKeymaster(&keymasterChannel);
+
+    if (!remoteKeymaster->Initialize()) {
+      LOG(FATAL) << "Could not initialize keymaster";
     }
 
     auto keymaster = new ::keymaster::V4_0::RemoteKeymaster4Device(remoteKeymaster);

msft-mirror-aosp/device.google.cuttlefish

(cherry picked from commit 9a5c369384e48f133002fae2edaaf0a26e6894a8)
Updated for `namespace cuttlefish`.

guest/hals/keymaster/remote/remote_keymaster.cpp
@@ -16,15 +16,42 @@
 
 #include "remote_keymaster.h"
 
-#include <cutils/log.h>
+#include <android-base/logging.h>
 #include <keymaster/android_keymaster_messages.h>
 #include <keymaster/keymaster_configuration.h>
 #include "common/libs/security/keymaster_ipc.h"
 
 namespace keymaster {
 
-int RemoteKeymaster::Initialize() {
-    // TODO(schuffelen): Connect to the remote side.
+RemoteKeymaster::RemoteKeymaster(cuttlefish::KeymasterChannel* channel)
+    : channel_(channel) {}
+
+RemoteKeymaster::~RemoteKeymaster() {
+}
+
+void RemoteKeymaster::ForwardCommand(enum keymaster_command command, const Serializable& req,
+                                     KeymasterResponse* rsp) {
+    if (!channel_->SendMessage(command, req)) {
+        LOG(ERROR) << "Failed to send keymaster message: " << command;
+        rsp->error = KM_ERROR_UNKNOWN_ERROR;
+        return;
+    }
+    auto response = channel_->ReceiveMessage();
+    if (!response) {
+        LOG(ERROR) << "Failed to receive keymaster response: " << command;
+        rsp->error = KM_ERROR_UNKNOWN_ERROR;
+        return;
+    }
+    const uint8_t* buffer = response->payload;
+    const uint8_t* buffer_end = response->payload + response->payload_size;
+    if (!rsp->Deserialize(&buffer, buffer_end)) {
+        LOG(ERROR) << "Failed to deserialize keymaster response: " << command;
+        rsp->error = KM_ERROR_UNKNOWN_ERROR;
+        return;
+    }
+}
+
+bool RemoteKeymaster::Initialize() {
 
     ConfigureRequest req;
     req.os_version = GetOsVersion();
@@ -34,30 +61,11 @@ int RemoteKeymaster::Initialize() {
     Configure(req, &rsp);
 
     if (rsp.error != KM_ERROR_OK) {
-        ALOGE("Failed to configure keymaster %d", rsp.error);
-        return -1;
+        LOG(ERROR) << "Failed to configure keymaster: " << rsp.error;
+        return false;
     }
 
-    return 0;
-}
-
-RemoteKeymaster::RemoteKeymaster() {}
-
-RemoteKeymaster::~RemoteKeymaster() {
-    // TODO(schuffelen): Disconnect from the remote side.
-}
-
-static void ForwardCommand(enum keymaster_command command, const Serializable& req,
-                           KeymasterResponse* rsp) {
-    keymaster_error_t err = KM_ERROR_OK;
-    // TODO(schuffelen): Send the message to the remote side.
-    (void) command;
-    (void) req;
-    (void) rsp;
-    if (err != KM_ERROR_OK) {
-        ALOGE("Failed to send cmd %d err: %d", command, err);
-        rsp->error = err;
-    }
+    return true;
 }
 
 void RemoteKeymaster::GetVersion(const GetVersionRequest& request, GetVersionResponse* response) {

guest/hals/keymaster/remote/remote_keymaster.h
@@ -19,13 +19,20 @@
 
 #include <keymaster/android_keymaster_messages.h>
 
+#include "common/libs/security/keymaster_channel.h"
+
 namespace keymaster {
 
 class RemoteKeymaster {
+  private:
+    cuttlefish::KeymasterChannel* channel_;
+
+    void ForwardCommand(enum keymaster_command command, const Serializable& req,
+                        KeymasterResponse* rsp);
   public:
-    RemoteKeymaster();
+    RemoteKeymaster(cuttlefish::KeymasterChannel*);
     ~RemoteKeymaster();
-    int Initialize();
+    bool Initialize();
     void GetVersion(const GetVersionRequest& request, GetVersionResponse* response);
     void SupportedAlgorithms(const SupportedAlgorithmsRequest& request,
                              SupportedAlgorithmsResponse* response);

guest/hals/keymaster/remote/service4.cpp
@@ -17,17 +17,35 @@
 
 #include <android-base/logging.h>
 #include <android/hardware/keymaster/4.0/IKeymasterDevice.h>
+#include <cutils/properties.h>
+#include <gflags/gflags.h>
 #include <hidl/HidlTransportSupport.h>
+
+#include "common/libs/fs/shared_fd.h"
+#include "common/libs/security/keymaster_channel.h"
 #include <guest/hals/keymaster/remote/remote_keymaster.h>
 #include <guest/hals/keymaster/remote/remote_keymaster4_device.h>
 
-int main() {
+DEFINE_uint32(
+    port,
+    static_cast<uint32_t>(property_get_int64("ro.boot.vsock_keymaster_port", 0)),
+    "virtio socket port to send keymaster commands to");
+
+int main(int argc, char** argv) {
+    ::android::base::InitLogging(argv);
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
     ::android::hardware::configureRpcThreadpool(1, true);
-    auto remoteKeymaster = new keymaster::RemoteKeymaster();
-    int err = remoteKeymaster->Initialize();
-    if (err != 0) {
-        LOG(FATAL) << "Could not initialize RemoteKeymaster (" << err << ")";
-        return -1;
+
+    auto vsockFd = cuttlefish::SharedFD::VsockClient(2, FLAGS_port, SOCK_STREAM);
+    if (!vsockFd->IsOpen()) {
+        LOG(FATAL) << "Could not connect to keymaster server: "
+                   << vsockFd->StrError();
+    }
+    cuttlefish::KeymasterChannel keymasterChannel(vsockFd);
+    auto remoteKeymaster = new keymaster::RemoteKeymaster(&keymasterChannel);
+
+    if (!remoteKeymaster->Initialize()) {
+      LOG(FATAL) << "Could not initialize keymaster";
     }
 
     auto keymaster = new ::keymaster::V4_0::RemoteKeymaster4Device(remoteKeymaster);