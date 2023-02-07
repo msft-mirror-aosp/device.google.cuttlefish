google/android-cuttlefish

Restructure disk choice logic
Test: launch_cvd
Bug: 241484900
Change-Id: Idf64920ea4aa8684328a761432bb85b639572a0d

host/commands/assemble_cvd/flags.cc
@@ -857,26 +857,35 @@ CuttlefishConfig InitializeCuttlefishConfiguration(
 
     instance.set_camera_server_port(FLAGS_camera_server_port);
 
-    if (FLAGS_protected_vm) {
-      instance.set_virtual_disk_paths(
-          {const_instance.PerInstancePath("os_composite.img")});
+    std::vector<std::string> virtual_disk_paths;
+
+    bool os_overlay = true;
+    os_overlay &= !FLAGS_protected_vm;
+    // Gem5 already uses CoW wrappers around disk images
+    os_overlay &= FLAGS_vm_manager != Gem5Manager::name();
+    if (os_overlay) {
+      auto path = const_instance.PerInstancePath("overlay.img");
+      virtual_disk_paths.push_back(path);
     } else {
-      std::vector<std::string> virtual_disk_paths = {
-          const_instance.PerInstancePath("persistent_composite.img"),
-      };
-      if (FLAGS_vm_manager != Gem5Manager::name()) {
-        virtual_disk_paths.insert(virtual_disk_paths.begin(),
-            const_instance.PerInstancePath("overlay.img"));
-      } else {
-        // Gem5 already uses CoW wrappers around disk images
-        virtual_disk_paths.insert(virtual_disk_paths.begin(),
-            const_instance.os_composite_disk_path());
-      }
-      if (FLAGS_use_sdcard) {
-        virtual_disk_paths.push_back(const_instance.sdcard_path());
-      }
-      instance.set_virtual_disk_paths(virtual_disk_paths);
+      virtual_disk_paths.push_back(const_instance.os_composite_disk_path());
     }
+
+    bool persistent_disk = true;
+    persistent_disk &= !FLAGS_protected_vm;
+    persistent_disk &= FLAGS_vm_manager != Gem5Manager::name();
+    if (persistent_disk) {
+      auto path = const_instance.PerInstancePath("persistent_composite.img");
+      virtual_disk_paths.push_back(path);
+    }
+
+    bool sdcard = true;
+    sdcard &= FLAGS_use_sdcard;
+    sdcard &= !FLAGS_protected_vm;
+    if (sdcard) {
+      virtual_disk_paths.push_back(const_instance.sdcard_path());
+    }
+
+    instance.set_virtual_disk_paths(virtual_disk_paths);
 
     // We'd like to set mac prefix to be 5554, 5555, 5556, ... in normal cases.
     // When --base_instance_num=3, this might be 5556, 5557, 5558, ... (skipping

msft-mirror-aosp/device.google.cuttlefish

Bug: 260641823

host/commands/assemble_cvd/flags.cc
@@ -798,26 +798,35 @@ CuttlefishConfig InitializeCuttlefishConfiguration(
 
     instance.set_camera_server_port(FLAGS_camera_server_port);
 
-    if (FLAGS_protected_vm) {
-      instance.set_virtual_disk_paths(
-          {const_instance.PerInstancePath("os_composite.img")});
+    std::vector<std::string> virtual_disk_paths;
+
+    bool os_overlay = true;
+    os_overlay &= !FLAGS_protected_vm;
+    // Gem5 already uses CoW wrappers around disk images
+    os_overlay &= FLAGS_vm_manager != Gem5Manager::name();
+    if (os_overlay) {
+      auto path = const_instance.PerInstancePath("overlay.img");
+      virtual_disk_paths.push_back(path);
     } else {
-      std::vector<std::string> virtual_disk_paths = {
-          const_instance.PerInstancePath("persistent_composite.img"),
-      };
-      if (FLAGS_vm_manager != Gem5Manager::name()) {
-        virtual_disk_paths.insert(virtual_disk_paths.begin(),
-            const_instance.PerInstancePath("overlay.img"));
-      } else {
-        // Gem5 already uses CoW wrappers around disk images
-        virtual_disk_paths.insert(virtual_disk_paths.begin(),
-            tmp_config_obj.os_composite_disk_path());
-      }
-      if (FLAGS_use_sdcard) {
-        virtual_disk_paths.push_back(const_instance.sdcard_path());
-      }
-      instance.set_virtual_disk_paths(virtual_disk_paths);
+      virtual_disk_paths.push_back(tmp_config_obj.os_composite_disk_path());
     }
+
+    bool persistent_disk = true;
+    persistent_disk &= !FLAGS_protected_vm;
+    persistent_disk &= FLAGS_vm_manager != Gem5Manager::name();
+    if (persistent_disk) {
+      auto path = const_instance.PerInstancePath("persistent_composite.img");
+      virtual_disk_paths.push_back(path);
+    }
+
+    bool sdcard = true;
+    sdcard &= FLAGS_use_sdcard;
+    sdcard &= !FLAGS_protected_vm;
+    if (sdcard) {
+      virtual_disk_paths.push_back(const_instance.sdcard_path());
+    }
+
+    instance.set_virtual_disk_paths(virtual_disk_paths);
 
     // We'd like to set mac prefix to be 5554, 5555, 5556, ... in normal cases.
     // When --base_instance_num=3, this might be 5556, 5557, 5558, ... (skipping