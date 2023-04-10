google/android-cuttlefish

Change dir to HOME to match fetcher_config.json
When `launch_cvd` and `assemble_cvd` are executed from the users
home directory, such as `/home/vsoc-01`, they will not locate a
staged `fetcher_config.json` file located one level down in a cvd
instance directory, such as `/home/vsoc-01/acloud_cf_1`. It can't
find it even if the `$HOME` environment variable it set ahead of
time, which is typically done similar to:
```
HOME=$HOME/acloud_cf_1 acloud_cf_1/bin/launch_cvd
```

This breaks the `fetch_cvd` to `launch_cvd`/`assemble_cvd` hand off
of files to use, such as specific kernel image files used for mixed
build scenarios.

The solution of this change is to continue to look for the
`fetcher_config.json` file first in the current directory, if it's not
present then also look in the $HOME directory. For `assemble_cvd`, if
it's located in $HOME change the current working directory to the
$HOME directory in order for all paths inside of the
`fetcher_config.json` to be valid.

Bug: 262275345
Test: Ran launch_cvd in and out of base directory.
Change-Id: Ie9939a8fd380f56593e9f9ff3402295f60a15e30
Signed-off-by: Joe Fradley <joefradley@google.com>

host/commands/assemble_cvd/assemble_cvd.cc
@@ -60,17 +60,33 @@ namespace {
 
 std::string kFetcherConfigFile = "fetcher_config.json";
 
-FetcherConfig FindFetcherConfig(const std::vector<std::string>& files) {
+struct LocatedFetcherConfig {
   FetcherConfig fetcher_config;
+  std::optional<std::string> working_dir;
+};
+
+LocatedFetcherConfig FindFetcherConfig(const std::vector<std::string>& files) {
+  LocatedFetcherConfig located_fetcher_config;
   for (const auto& file : files) {
     if (android::base::EndsWith(file, kFetcherConfigFile)) {
-      if (fetcher_config.LoadFromFile(file)) {
-        return fetcher_config;
+      std::string home_directory = StringFromEnv("HOME", CurrentDirectory());
+      std::string fetcher_file = file;
+      if (!FileExists(file) &&
+          FileExists(home_directory + "/" + fetcher_file)) {
+        LOG(INFO) << "Found " << fetcher_file << " in HOME directory ('"
+                  << home_directory << "') and not current working directory";
+
+        located_fetcher_config.working_dir = home_directory;
+        fetcher_file = home_directory + "/" + fetcher_file;
+      }
+
+      if (located_fetcher_config.fetcher_config.LoadFromFile(fetcher_file)) {
+        return located_fetcher_config;
       }
       LOG(ERROR) << "Could not load fetcher config file.";
     }
   }
-  return fetcher_config;
+  return located_fetcher_config;
 }
 
 std::string GetLegacyConfigFilePath(const CuttlefishConfig& config) {
@@ -351,9 +367,18 @@ Result<int> AssembleCvdMain(int argc, char** argv) {
   }
   std::vector<std::string> input_files = android::base::Split(input_files_str, "\n");
 
-  FetcherConfig fetcher_config = FindFetcherConfig(input_files);
+  LocatedFetcherConfig located_fetcher_config = FindFetcherConfig(input_files);
+  if (located_fetcher_config.working_dir) {
+    LOG(INFO) << "Changing current working dircetory to '"
+              << *located_fetcher_config.working_dir << "'";
+    CF_EXPECT(chdir((*located_fetcher_config.working_dir).c_str()) == 0,
+              "Unable to change working dir to '"
+                  << *located_fetcher_config.working_dir
+                  << "': " << strerror(errno));
+  }
+
   // set gflags defaults to point to kernel/RD from fetcher config
-  ExtractKernelParamsFromFetcherConfig(fetcher_config);
+  ExtractKernelParamsFromFetcherConfig(located_fetcher_config.fetcher_config);
 
   auto args = ArgsToVec(argc - 1, argv + 1);
 
@@ -404,10 +429,10 @@ Result<int> AssembleCvdMain(int argc, char** argv) {
   auto guest_configs =
       CF_EXPECT(GetGuestConfigAndSetDefaults(), "Failed to parse arguments");
 
-  auto config =
-      CF_EXPECT(InitFilesystemAndCreateConfig(std::move(fetcher_config),
-                                              guest_configs, injector),
-                "Failed to create config");
+  auto config = CF_EXPECT(InitFilesystemAndCreateConfig(
+                              std::move(located_fetcher_config.fetcher_config),
+                              guest_configs, injector),
+                          "Failed to create config");
 
   std::cout << GetConfigFilePath(*config) << "\n";
   std::cout << std::flush;

msft-mirror-aosp/device.google.cuttlefish

Bug: 275758570
Merged-In: Ie9939a8fd380f56593e9f9ff3402295f60a15e30
(cherry picked from commit c7c692ddc7afb271172f7a535740d915633f3bd1)

host/commands/assemble_cvd/assemble_cvd.cc
@@ -56,17 +56,33 @@ namespace {
 
 std::string kFetcherConfigFile = "fetcher_config.json";
 
-FetcherConfig FindFetcherConfig(const std::vector<std::string>& files) {
+struct LocatedFetcherConfig {
   FetcherConfig fetcher_config;
+  std::optional<std::string> working_dir;
+};
+
+LocatedFetcherConfig FindFetcherConfig(const std::vector<std::string>& files) {
+  LocatedFetcherConfig located_fetcher_config;
   for (const auto& file : files) {
     if (android::base::EndsWith(file, kFetcherConfigFile)) {
-      if (fetcher_config.LoadFromFile(file)) {
-        return fetcher_config;
+      std::string home_directory = StringFromEnv("HOME", CurrentDirectory());
+      std::string fetcher_file = file;
+      if (!FileExists(file) &&
+          FileExists(home_directory + "/" + fetcher_file)) {
+        LOG(INFO) << "Found " << fetcher_file << " in HOME directory ('"
+                  << home_directory << "') and not current working directory";
+
+        located_fetcher_config.working_dir = home_directory;
+        fetcher_file = home_directory + "/" + fetcher_file;
+      }
+
+      if (located_fetcher_config.fetcher_config.LoadFromFile(fetcher_file)) {
+        return located_fetcher_config;
       }
       LOG(ERROR) << "Could not load fetcher config file.";
     }
   }
-  return fetcher_config;
+  return located_fetcher_config;
 }
 
 std::string GetLegacyConfigFilePath(const CuttlefishConfig& config) {
@@ -329,9 +345,18 @@ Result<int> AssembleCvdMain(int argc, char** argv) {
   }
   std::vector<std::string> input_files = android::base::Split(input_files_str, "\n");
 
-  FetcherConfig fetcher_config = FindFetcherConfig(input_files);
+  LocatedFetcherConfig located_fetcher_config = FindFetcherConfig(input_files);
+  if (located_fetcher_config.working_dir) {
+    LOG(INFO) << "Changing current working dircetory to '"
+              << *located_fetcher_config.working_dir << "'";
+    CF_EXPECT(chdir((*located_fetcher_config.working_dir).c_str()) == 0,
+              "Unable to change working dir to '"
+                  << *located_fetcher_config.working_dir
+                  << "': " << strerror(errno));
+  }
+
   // set gflags defaults to point to kernel/RD from fetcher config
-  ExtractKernelParamsFromFetcherConfig(fetcher_config);
+  ExtractKernelParamsFromFetcherConfig(located_fetcher_config.fetcher_config);
 
   auto args = ArgsToVec(argc - 1, argv + 1);
 
@@ -377,10 +402,10 @@ Result<int> AssembleCvdMain(int argc, char** argv) {
   auto kernel_config =
       CF_EXPECT(GetKernelConfigAndSetDefaults(), "Failed to parse arguments");
 
-  auto config =
-      CF_EXPECT(InitFilesystemAndCreateConfig(std::move(fetcher_config),
-                                              kernel_config, injector),
-                "Failed to create config");
+  auto config = CF_EXPECT(InitFilesystemAndCreateConfig(
+                              std::move(located_fetcher_config.fetcher_config),
+                              kernel_config, injector),
+                          "Failed to create config");
 
   std::cout << GetConfigFilePath(*config) << "\n";
   std::cout << std::flush;