2393df1e76fd852f63cd6f1a9ae9cb2c105f5b89
Carlos Llamas <cmllamas@google.com>
2025-12-22 20:04:56 +0000

    arm64: disable erofs_file_backed_mount
    This feature breaks the 'main_cf_arm64_boot_test' as several loop device
    boot issues are seen in GKI kernels with this change, e.g.:
    
      erofs (device erofs): cannot find valid erofs superblock
      [...]
    
    Lets temporarily disable it until the issue is properly root-caused and
    fixed.
    
    Bug: 465953130
    Bug: 469875222
    Cc: Cheng-Cheng Lo <locc@google.com>
    Change-Id: I3f169cbe91bc0bedbfcdfc6ea3f96ac7f0f33bdb
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- vsoc_arm64_pgagnostic/phone/aosp_cf.mk
+++ vsoc_arm64_pgagnostic/phone/aosp_cf.mk
@@ -71,3 +71,6 @@ TARGET_BOOTS_16K := true
 
 # Ignore all Android.mk files
 PRODUCT_IGNORE_ALL_ANDROIDMK := true
+
+# TODO(b/469875222): This property is temporarily disabled to unblock other testing.
+PRODUCT_VENDOR_PROPERTIES += apexd.config.erofs_file_backed_mount=false
