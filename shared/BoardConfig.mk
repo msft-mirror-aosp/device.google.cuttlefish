138cc1bf1dd9889e5ef94cb05b6e490d0c27b531
A. Cody Schuffelen <schuffelen@google.com>
2026-01-12 12:47:45 -0800

    Create VARIANT_DIR setup comparable to vvmtruststore to fix coverage build
    See b/473875536#comment7 for the comparison.
    
    Bug: b/473875536
    Change-Id: Ie7f41d7527cca4217273cc2411c759fed5dc9077
    Test: TreeHugger

--- shared/BoardConfig.mk
+++ shared/BoardConfig.mk
@@ -499,4 +499,10 @@ endif
 
 BOARD_CUSTOMIMAGES_PARTITION_LIST += cuttlefish_example_custom
 
-BOARD_CUTTLEFISH_EXAMPLE_CUSTOM_IMAGE_LIST := $(OUT_DIR)/soong/.intermediates/device/google/cuttlefish/custom_partition/cuttlefish_example_custom/android_common/cuttlefish_example_custom.img
+ifeq ($(CLANG_COVERAGE),true)
+  VARIANT_DIR = android_common_cov
+else
+  VARIANT_DIR = android_common
+endif
+
+BOARD_CUTTLEFISH_EXAMPLE_CUSTOM_IMAGE_LIST := $(OUT_DIR)/soong/.intermediates/device/google/cuttlefish/custom_partition/cuttlefish_example_custom/$(VARIANT_DIR)/cuttlefish_example_custom.img
