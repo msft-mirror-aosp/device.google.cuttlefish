defefa8729bd446cdcf9ebbec285762726e0fcf2
Aleksei Vetrov <vvvvvv@google.com>
2026-01-15 08:57:28 -0800

    Guard AiSeal/AppSearch configuration with a separate build flag
    Add a flag for AiSeal with AppSearch integration to control the rollout
    of this feature.
    
    Bug: 465465109
    Test: atest CtsSecurityHostTestCases
    Flag: build.RELEASE_AISEAL_WITH_APPSEARCH
    Change-Id: I88137f26f94a4d14386f4061206df5121a20688e

--- shared/aiseal/device_vendor.mk
+++ shared/aiseal/device_vendor.mk
@@ -19,6 +19,7 @@
 ifeq ($(RELEASE_AISEAL_FRAMEWORK),true)
 PRODUCT_PACKAGES += android.software.aiseal.prebuilt.xml
 
+ifeq ($(RELEASE_AISEAL_WITH_APPSEARCH),true)
 # Enable AiSeal with AppSearch.
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.appsearch.feature.enable_aiseal=1 \
@@ -26,7 +27,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 	service.aiseal.tenant_config_package=com.android.appsearch.aiseal.config \
 	service.aiseal.tenant_config_path=assets/vm_config.json \
 	service.aiseal.aiseal_config_path=assets/aiseal_config.json
+endif  # RELEASE_AISEAL_WITH_APPSEARCH
 
 # Protected VM is not supported in Cuttlefish, use nonprotected VM instead.
 PRODUCT_PRODUCT_PROPERTIES += service.aiseal.protected_vm=0
-endif
+endif  # RELEASE_AISEAL_FRAMEWORK
