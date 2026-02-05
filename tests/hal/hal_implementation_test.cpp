50f0d8039a98a2cc28af6776e2d7457516f87bab
Steven Moreland <smoreland@google.com>
2026-02-05 13:15:14 -0800

    hal_implementation_test: no HIDL
    No more HIDL HALs are allowed, so clear out these dependencies.
    
    Bug: 482083752
    Test: N/A
    Change-Id: If34a5c91ee7c4eecd02d4c49a506f9ab37e0ff8c

--- tests/hal/hal_implementation_test.cpp
+++ tests/hal/hal_implementation_test.cpp
@@ -21,8 +21,6 @@
 #include <android/content/pm/IPackageManagerNative.h>
 #include <binder/IServiceManager.h>
 #include <gtest/gtest.h>
-#include <hidl-util/FQName.h>
-#include <hidl/metadata.h>
 #include <vintf/VintfObject.h>
 
 #include <algorithm>
@@ -40,112 +38,6 @@ constexpr bool kAidlUseUnfrozen = false;
 #endif
 
 using namespace android;
-
-// clang-format off
-static const std::set<std::string> kKnownMissingHidl = {
-    "android.frameworks.automotive.display@1.0", // converted to AIDL, see b/170401743
-    "android.frameworks.cameraservice.device@2.1",
-    "android.frameworks.cameraservice.service@2.2", // converted to AIDL, see b/205764761
-    "android.frameworks.displayservice@1.0", // deprecated, see b/141930622
-    "android.frameworks.schedulerservice@1.0", // deprecated, see b/37226359
-    "android.frameworks.sensorservice@1.0", // deprecated, see b/205764765
-    "android.frameworks.vr.composer@1.0",
-    "android.frameworks.vr.composer@2.0",
-    "android.frameworks.stats@1.0",  // converted to AIDL, see b/177667419
-    "android.hardware.atrace@1.0", // deprecated, see b/204935495
-    "android.hardware.audio@2.0",
-    "android.hardware.audio@4.0",
-    "android.hardware.audio@5.0",
-    "android.hardware.audio@6.0",
-    "android.hardware.audio@7.1", // converted to AIDL, see b/264712385
-    "android.hardware.audio.effect@2.0",
-    "android.hardware.audio.effect@4.0",
-    "android.hardware.audio.effect@5.0",
-    "android.hardware.audio.effect@6.0",
-    "android.hardware.audio.effect@7.0", // converted to AIDL, see b/264712385
-    "android.hardware.authsecret@1.0", // converted to AIDL, see b/182976659
-    "android.hardware.automotive.can@1.0",  // converted to AIDL, see b/170405615
-    "android.hardware.automotive.evs@1.1",
-    "android.hardware.automotive.sv@1.0",
-    "android.hardware.automotive.vehicle@2.0",
-    "android.hardware.biometrics.fingerprint@2.3", // converted to AIDL, see b/152416783
-    "android.hardware.biometrics.face@1.0", // converted to AIDL, see b/168730443
-    "android.hardware.bluetooth.a2dp@1.0",
-    "android.hardware.bluetooth.audio@2.1", // converted to AIDL, see b/203490261
-    "android.hardware.bluetooth@1.1", // converted to AIDL, see b/205758693
-    "android.hardware.boot@1.2", // converted to AIDL, see b/227536004
-    "android.hardware.broadcastradio@1.1",
-    "android.hardware.broadcastradio@2.0",
-    "android.hardware.camera.provider@2.7", // Camera converted to AIDL, b/196432585
-    "android.hardware.cas@1.2", // converted to AIDL, see b/227673974
-    "android.hardware.cas.native@1.0",
-    "android.hardware.configstore@1.1", // deprecated, see b/149050985, b/149050733
-    "android.hardware.confirmationui@1.0", // converted to AIDL, see b/205760172
-    "android.hardware.contexthub@1.2",
-    "android.hardware.drm@1.4", // converted to AIDL, b/200055138
-    "android.hardware.fastboot@1.1",
-    "android.hardware.dumpstate@1.1", // deprecated, see b/205760700
-    "android.hardware.gatekeeper@1.0", // converted to AIDL, b/205760843
-    "android.hardware.gnss@1.1", // GNSS converted to AIDL, b/206670536
-    "android.hardware.gnss@2.1", // GNSS converted to AIDL, b/206670536
-    "android.hardware.gnss.measurement_corrections@1.1", // is sub-interface of gnss
-    "android.hardware.gnss.visibility_control@1.0",
-    "android.hardware.graphics.allocator@2.0",
-    "android.hardware.graphics.allocator@3.0",
-    "android.hardware.graphics.allocator@4.0", // converted to AIDL, see b/205761012
-    "android.hardware.graphics.bufferqueue@1.0",
-    "android.hardware.graphics.bufferqueue@2.0",
-    "android.hardware.graphics.composer@2.4", // converted to AIDL, see b/193240715
-    "android.hardware.graphics.mapper@2.1",
-    "android.hardware.graphics.mapper@3.0",
-    "android.hardware.graphics.mapper@4.0", // converted to Stable C, see b/205761028
-    "android.hardware.health.storage@1.0", // converted to AIDL, see b/177470478
-    "android.hardware.health@2.1", // converted to AIDL, see b/177269435
-    "android.hardware.input.classifier@1.0", // converted to AIDL, see b/205761620
-    "android.hardware.ir@1.0", // converted to AIDL, see b/205000342
-    "android.hardware.keymaster@3.0",
-    "android.hardware.keymaster@4.1", // Replaced by AIDL KeyMint, see b/111446262
-    "android.hardware.light@2.0",
-    "android.hardware.media.bufferpool@1.0",
-    "android.hardware.media.bufferpool@2.0",
-    "android.hardware.media.omx@1.0", // deprecated b/205761766
-    "android.hardware.memtrack@1.0",
-    "android.hardware.neuralnetworks@1.3", // converted to AIDL, see b/161428342
-    "android.hardware.nfc@1.2",
-    "android.hardware.oemlock@1.0", // converted to AIDL, see b/176107318 b/282160400
-    "android.hardware.power@1.3",
-    "android.hardware.power.stats@1.0",
-    "android.hardware.radio@1.6", // converted to AIDL
-    "android.hardware.radio.config@1.3", // converted to AIDL
-    "android.hardware.radio.deprecated@1.0",
-    "android.hardware.renderscript@1.0",
-    "android.hardware.soundtrigger@2.3",
-    "android.hardware.secure_element@1.2",
-    "android.hardware.sensors@1.0",
-    "android.hardware.sensors@2.1",
-    "android.hardware.tetheroffload.config@1.0",
-    "android.hardware.tetheroffload.control@1.1", // see b/170699770
-    "android.hardware.thermal@1.1",
-    "android.hardware.thermal@2.0", // Converted to AIDL (see b/205762943)
-    "android.hardware.tv.cec@1.1",
-    "android.hardware.tv.input@1.0",
-    "android.hardware.tv.tuner@1.1",
-    "android.hardware.usb@1.3", // migrated to AIDL see b/200993386
-    "android.hardware.usb.gadget@1.2",
-    "android.hardware.vibrator@1.3",
-    "android.hardware.vr@1.0",
-    "android.hardware.weaver@1.0",
-    "android.hardware.wifi@1.6", // Converted to AIDL (see b/205044134)
-    "android.hardware.wifi.hostapd@1.3", // Converted to AIDL (see b/194806512)
-    "android.hardware.wifi.supplicant@1.4", // Converted to AIDL (see b/196235436)
-    "android.hidl.base@1.0",
-    "android.hidl.memory.token@1.0",
-    "android.hidl.memory@1.0", // Deprecated (see b/205764958)
-    "android.system.net.netd@1.1", // Converted to AIDL (see b/205764585)
-    "android.system.suspend@1.0", // Converted to AIDL (see b/170260236)
-    "android.system.wifi.keystore@1.0", // Converted to AIDL (see b/205764502)
-};
-// clang-format on
 
 struct VersionedAidlPackage {
   std::string name;
