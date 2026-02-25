f72dc8929af3444d6658aa1fa3b6b98fedad0a7a
Jorge E. Moreira <jemoreira@google.com>
2026-02-25 15:14:11 -0800

    Make sure to follow symlinks when reading files
    The android::base::ReadFileToString has a third parameter that controls
    whether to fail if the file is a symbolic link. Unfortunately the
    default behavior is to fail instead of following the link. This cl adds
    the parameter everywhere.
    
    Bug: 487770769
    Flag: EXEMPT HOST_MODE
    Test: locally
    Change-Id: Ia8589ea54f7bc733c497d02c78f7b04eaf82554e

--- host/libs/config/config_flag.cpp
+++ host/libs/config/config_flag.cpp
@@ -94,8 +94,9 @@ class ConfigReader : public FlagFeature {
     auto path =
         DefaultHostArtifactsPath("etc/cvd_config/cvd_config_" + name + ".json");
     std::string config_contents;
-    CF_EXPECTF(android::base::ReadFileToString(path, &config_contents),
-               "Could not read config file \"{}\"", path);
+    CF_EXPECTF(
+        ReadFileToString(path, &config_contents, /* follow_symlinks */ true),
+        "Could not read config file \"{}\"", path);
     return CF_EXPECTF(ParseJson(config_contents),
                       "Could not parse config file \"{}\"", path);
   }
@@ -216,7 +217,8 @@ class ConfigFlagImpl : public ConfigFlag {
       return {};
     }
     std::string android_info;
-    if (!ReadFileToString(info_path, &android_info)) {
+    if (!ReadFileToString(info_path, &android_info,
+                          /* follow_symlinks */ true)) {
       return {};
     }
     // grab the config with name "config" in android-info.txt,
