62999a1494650b4cc2de628836726c744d42ab11
Aaqib Ismail <aaqibismail@google.com>
2026-03-08 16:29:32 -0700

    Move EVS HAL from missing to Automotive only
    Flag: EXEMPT TEST_ONLY
    Bug: 447677994
    Test: atest hal_implementation_test
    Change-Id: I0f8b68a73dd4b52b2a68bf1ee9ddf78c32de73a6

--- tests/hal/hal_implementation_test.cpp
+++ tests/hal/hal_implementation_test.cpp
@@ -66,6 +66,7 @@ static const std::set<std::string> kAutomotiveOnlyAidl = {
     "android.frameworks.automotive.telemetry",
     "android.hardware.automotive.audiocontrol",
     "android.hardware.automotive.can",
+    "android.hardware.automotive.evs",
     "android.hardware.broadcastradio",
     "android.hardware.automotive.occupant_awareness",
     "android.hardware.automotive.remoteaccess",
@@ -169,7 +170,6 @@ static const std::vector<VersionedAidlPackage> kKnownMissingAidl = {
 
     {"android.automotive.computepipe.registry.", 2, 273549907},
     {"android.automotive.computepipe.runner.", 2, 273549907},
-    {"android.hardware.automotive.evs.", 2, 274162534},
     {"android.hardware.security.see.authmgr.", 1, 379940224},
     // TODO(b/466983803): Remove this after implementing Trusted Hals on CF.
     {"android.hardware.security.see.devicestate.", 1, 466983803},
