390a9a3d272fd1dcefc388c37dbdd5dfd447402e
A. Cody Schuffelen <schuffelen@google.com>
2026-03-12 20:35:35 -0700

    Fix mixing of both kernel and super image
    Sometimes the `instance/super.img` file is already generated by the
    super mixing code, and does not need to be copied in from the input
    source files again.
    
    Bug: b/491001619
    Change-Id: Ie6e8c5000119a0498144104459696392f2bfe703
    Flag: EXEMPT cuttlefish host code
    Test: cvd load with system, vendor, and kernel build

--- host/commands/assemble_cvd/disk/kernel_ramdisk_repacker.cpp
+++ host/commands/assemble_cvd/disk/kernel_ramdisk_repacker.cpp
@@ -72,9 +72,11 @@ Result<void> RepackSuperAndVbmeta(
              system_dlkm_build_dir);
 
   const auto new_super_img = instance.new_super_image();
-  CF_EXPECTF(Copy(instance.super_image(), new_super_img),
-             "Failed to copy super image '{}' to '{}': '{}'",
-             instance.super_image(), new_super_img, strerror(errno));
+  if (!FileExists(new_super_img)) {
+    CF_EXPECTF(Copy(instance.super_image(), new_super_img),
+               "Failed to copy super image '{}' to '{}': '{}'",
+               instance.super_image(), new_super_img, strerror(errno));
+  }
 
   CF_EXPECT(RepackSuperWithPartition(new_super_img, new_vendor_dlkm_img,
                                      "vendor_dlkm"),
