80cceea0802b22bf9e12bc2c315fe54a1229d5c0
Augustin Lassus <augustinlassus@google.com>
2026-03-30 15:28:41 +0000

    Optimize hibernation swap provisioning
    Implemented the following changes:
    
    - Introduced ENABLE_HIBERNATION_SWAP flag. Enabled by default for targets with "auto"
    - Switched to sparse file creation: replaced dd command with truncate -s, making image creation instantaneous. dd was writing 4gb of zeros to disk where truncate only updates the filesystem's metadata to record that the file should be 4gb.
    - guarded the ro.sys.hibernate_enabled, ro.sys.swap_storage_device properties, and fstab.hibernationswap copy, with the ENABLE_HIBERNATION_SWAP flag.
    
    Bug: 423699294
    Test: manually checked by running time lunch cf_x86_64_auto-trunk_staging-userdebug
    
    Change-Id: I61dd808e0049e8b440d5d27cc6afc3877df0a038

--- shared/auto/device_vendor.mk
+++ shared/auto/device_vendor.mk
@@ -55,10 +55,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.uwbcountrycode=US
 
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.sys.hibernate_enabled=1 \
-    ro.sys.swap_storage_device=/dev/block/vda19
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/car_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/car_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.broadcastradio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.broadcastradio.xml \
@@ -79,9 +75,18 @@ LOCAL_HEALTH_PRODUCT_PACKAGE := \
 PRODUCT_COPY_FILES += \
     device/google/cuttlefish/shared/auto/display_settings.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display_settings.xml
 
-# Include the fstab needed for suspend to disk
+ENABLE_HIBERNATION_SWAP ?= false
+ifeq ($(ENABLE_HIBERNATION_SWAP), true)
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sys.hibernate_enabled=1 \
+    ro.sys.swap_storage_device=/dev/block/vda19
+
 PRODUCT_COPY_FILES += \
     device/google/cuttlefish/shared/auto/hibernation_swap/fstab.hibernationswap:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.hibernationswap
+
+# Ensure the hibernation swap image is built
+droidcore: $(PRODUCT_OUT)/hibernation_swap.img
+endif
 
 # vehicle HAL
 ifeq ($(LOCAL_VHAL_PRODUCT_PACKAGE),)
