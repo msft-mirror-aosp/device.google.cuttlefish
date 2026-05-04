2d8de3722a42fd3607c750e509ae5ab541d4bc3f
Brian Daniels <briandaniels@google.com>
2026-05-04 13:59:23 -0400

    Move wpa_supplicant_macsec service into auto_eth ns.
    When the `auto_eth` namespace is not in use, the wpa_supplicant_macsec
    service remains out of the namespace.
    
    Bug: 509554800
    Test: lunch cf_x86_64_auto-trunk_staging-userdebug && m && cvd create
          Check logcat, ensure the service starts correctly
    Test: lunch sdv_ivi_cf-trunk_staging-userdebug && m && cvd create
          Check logcat, ensure the service starts correctly
    Change-Id: I92ce13f2cbe99c4b1917fd81af09f4ae7e2ed1d9

--- shared/auto/device_vendor.mk
+++ shared/auto/device_vendor.mk
@@ -129,8 +129,15 @@ PRODUCT_PACKAGES += android.hardware.automotive.can-service
 PRODUCT_PACKAGES += android.hardware.macsec-service
 PRODUCT_PACKAGES += wpa_supplicant_macsec
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/macsec/wpa_supplicant_macsec.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wpa_supplicant_macsec.conf \
+    $(LOCAL_PATH)/macsec/wpa_supplicant_macsec.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wpa_supplicant_macsec.conf
+
+ifeq ($(ENABLE_AUTO_ETHERNET), true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/macsec/init.wpa_supplicant_macsec_ns.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.wpa_supplicant_macsec_ns.rc
+else
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/macsec/init.wpa_supplicant_macsec.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.wpa_supplicant_macsec.rc
+endif
 
 # Occupant Awareness HAL
 PRODUCT_PACKAGES += android.hardware.automotive.occupant_awareness@1.0-service
