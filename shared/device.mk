6ade9298c0427b49bcd6435ef4a79715acf53b3d
Neill Kapron <nkapron@google.com>
2026-05-08 18:34:43 +0000

    Remove USB accessory feature definition from Cuttlefish
    Cuttlefish does not support USB accessory mode (AOA), so we should not
    be declaring support for it at this time.
    
    Bug: 510347678
    Test: atest VtsAidlUsbHostTest
    Flag: EXEMPT BUGFIX
    Change-Id: I266920bcf4174c9bcb3f5215ec6c53fb97551a95

--- shared/device.mk
+++ shared/device.mk
@@ -246,7 +246,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
