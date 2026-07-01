a2ef8d3d0e34361a450c4126dc760104ee37d6cf
Jason Macnak <natsu@google.com>
2026-07-01 16:20:34 -0700

    Ignore unknown fields in graphics detector parsing
    ... to support when the graphics detector is substituted and
    assemble_cvd is not and when the graphics detector from github
    has new fields (such as the one added in
    https://github.com/google/android-cuttlefish/pull/2729).
    
    Also, adds error collecting to potentially help in the future.
    
    Bug: b/530288420
    Test: cvd create
    Change-Id: If647b68aea20f506dac9a8eaafc42f31f31b726f

--- host/commands/assemble_cvd/graphics_flags.cc
+++ host/commands/assemble_cvd/graphics_flags.cc
@@ -27,6 +27,7 @@
 #include <android-base/file.h>
 #include <android-base/strings.h>
 #include <fmt/format.h>
+#include <google/protobuf/io/tokenizer.h>
 #include <google/protobuf/text_format.h>
 
 #include "common/libs/utils/contains.h"
@@ -43,6 +44,23 @@
 
 namespace cuttlefish {
 namespace {
+
+struct AggregatingErrorCollector : public google::protobuf::io::ErrorCollector {
+  void RecordError(int /* line */, int /* column */,
+                   const absl::string_view message) override {
+    if (!error_message.empty()) {
+      error_message += "; ";
+    }
+    error_message += message;
+  }
+
+  void RecordWarning(int /* line */, int /* column */,
+                     const absl::string_view /* message */) override {
+    // Ignore warnings
+  }
+
+  std::string error_message;
+};
 
 struct CommonState {
   const VmmMode vmm_mode;
@@ -710,10 +728,15 @@ GetGraphicsAvailabilityWithSubprocessCheck() {
       graphics_availability_content_result.value();
 
   gfxstream::proto::GraphicsAvailability availability;
+
   google::protobuf::TextFormat::Parser parser;
+  parser.AllowUnknownField(true);
+  AggregatingErrorCollector error_collector;
+  parser.RecordErrorsTo(&error_collector);
   if (!parser.ParseFromString(graphics_availability_content, &availability)) {
     LOG(ERROR) << "Failed to parse graphics detector output: "
                << graphics_availability_content
+               << ". Error(s): " << error_collector.error_message
                << ". Assuming no availability.";
     return {};
   }
